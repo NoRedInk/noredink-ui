dfe92466aaaf1bed460f1db6a3e0e74f91dd1f94
Jasper Woudenberg <mail@jasperwoudenberg.com>
2017-12-07 16:38:39 +0100

    Revert "Remove `Nri.Alert`"
    We do need it now! Getting it back.
    
    This reverts commit 22ad1b3b7ef98c40cc903f75bfe5350a0a266819.

---
+++ src/Nri/Alert.elm
@@ -0,0 +1,148 @@
+module Nri.Alert
+    exposing
+        ( Model
+        , error
+        , styles
+        , success
+        , tip
+        , viewResult
+        , warning
+        )
+
+{-| UI components that highlight information to the user.
+
+@docs Model
+@docs error
+@docs styles
+@docs success
+@docs tip
+@docs viewResult
+@docs warning
+
+-}
+
+import Accessibility
+import Css
+import Css.Elements
+import Html exposing (Html)
+import Markdown
+import Nri.Colors
+import Nri.Styles
+
+
+{-| -}
+type alias Model =
+    { content : String }
+
+
+alert : CssClasses -> Model -> Html msg
+alert className { content } =
+    Accessibility.div
+        [ styles.class [ Alert, className ]
+        ]
+        [ Accessibility.div [] (Markdown.toHtml Nothing content)
+        ]
+
+
+{-| Show either an error or success alert depending on the given Result
+-}
+viewResult : Result String String -> Html msg
+viewResult result =
+    case result of
+        Ok msg ->
+            success { content = msg }
+
+        Err msg ->
+            error { content = msg }
+
+
+{-| -}
+error : Model -> Html msg
+error =
+    alert Error
+
+
+{-| -}
+success : Model -> Html msg
+success =
+    alert Success
+
+
+{-| -}
+tip : Model -> Html msg
+tip =
+    alert Tip
+
+
+{-| -}
+warning : Model -> Html msg
+warning =
+    alert Warning
+
+
+type CssClasses
+    = Alert
+    | Error
+    | Success
+    | Tip
+    | Warning
+
+
+{-| -}
+styles : Nri.Styles.Styles Never CssClasses msg
+styles =
+    Nri.Styles.styles "Nri-Alert-"
+        [ Css.class Alert
+            [ Css.displayFlex
+            , Css.fontSize (Css.px 13)
+            , Css.lineHeight (Css.num 1.2)
+            , Css.listStyleType Css.none
+            , Css.overflow Css.hidden
+            , Css.padding4 (Css.px 6) (Css.px 8) (Css.px 8) (Css.px 30)
+            , Css.position Css.relative
+            , Css.children
+                [ Css.Elements.div
+                    [ Css.children
+                        [ Css.Elements.p
+                            [ Css.margin Css.zero
+                            ]
+                        ]
+                    ]
+                ]
+            , Css.after
+                [ Css.backgroundPosition Css.center
+                , Css.backgroundRepeat Css.noRepeat
+                , Css.borderRadius (Css.px 13)
+                , Css.height (Css.px 25)
+                , Css.left Css.zero
+                , Css.position Css.absolute
+                , Css.property "content" "\"\""
+                , Css.top Css.zero
+                , Css.width (Css.px 25)
+                ]
+            ]
+        , Css.class Error
+            [ Css.color Nri.Colors.purple
+            , Css.after
+                [ Css.backgroundColor Nri.Colors.purple
+                ]
+            ]
+        , Css.class Success
+            [ Css.color Nri.Colors.greenDarkest
+            , Css.after
+                [ Css.backgroundColor Nri.Colors.green
+                ]
+            ]
+        , Css.class Tip
+            [ Css.color Nri.Colors.navy
+            , Css.after
+                [ Css.backgroundColor Nri.Colors.white
+                ]
+            ]
+        , Css.class Warning
+            [ Css.color Nri.Colors.red
+            , Css.after
+                [ Css.backgroundColor Nri.Colors.red
+                ]
+            ]
+        ]
