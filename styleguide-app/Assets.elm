6e041d9364aa50bb1c04be486350dc287e4703d0
Jasper Woudenberg <mail@jasperwoudenberg.com>
2018-03-29 12:58:41 +0200

    Styleguide uses latest Icon / SegmentedControl

---
+++ styleguide-app/Assets.elm
@@ -0,0 +1,151 @@
+module Assets exposing (Assets, assets)
+
+import Nri.Ui.AssetPath as AssetPath exposing (Asset(Asset))
+
+
+type alias Assets =
+    { activity : String
+    , arrowDown : String
+    , attention_svg : Asset
+    , bulb : String
+    , calendar : String
+    , checkmark : String
+    , class : String
+    , clever : String
+    , clock : String
+    , commentNotStarred_png : Asset
+    , commentStarred_png : Asset
+    , compass : String
+    , darkBlueCheckmark_svg : Asset
+    , diagnostic : String
+    , document : String
+    , download : String
+    , edit : String
+    , editWriting : String
+    , exclamation : String
+    , exclamationPoint_svg : Asset
+    , facebookBlue_svg : Asset
+    , flipper : String
+    , footsteps : String
+    , gear : String
+    , hint_png : Asset
+    , iconCalendar_svg : Asset
+    , iconCheck_png : Asset
+    , iconFlag_png : Asset
+    , icons_arrowDownBlue_svg : Asset
+    , icons_arrowRightBlue_svg : Asset
+    , icons_clockRed_svg : Asset
+    , icons_equals_svg : Asset
+    , icons_helpBlue_svg : Asset
+    , icons_peerReview_svg : Asset
+    , icons_plusBlue_svg : Asset
+    , icons_quickWrite_svg : Asset
+    , icons_searchGray_svg : Asset
+    , icons_xBlue_svg : Asset
+    , icons_xBlue_svg : Asset
+    , key : String
+    , leaderboard : String
+    , leftArrowBlue_png : Asset
+    , level1Badge_png : Asset
+    , level2Badge_png : Asset
+    , level3Badge_png : Asset
+    , lock : String
+    , logoRedBlack_svg : Asset
+    , newspaper : String
+    , openClose : String
+    , performance : String
+    , personBlue_svg : Asset
+    , practice : String
+    , premiumLock_svg : Asset
+    , preview : String
+    , quiz : String
+    , seemore : String
+    , share : String
+    , smallCheckmark_png : Asset
+    , sort : String
+    , sortArrow : String
+    , speedometer : String
+    , squiggly_png : Asset
+    , startingOffBadge_png : Asset
+    , teach_assignments_copyWhite_svg : Asset
+    , twitterBlue_svg : Asset
+    , unarchiveBlue2x_png : Asset
+    , writingAssignment : String
+    , writingcycle : String
+    , x : String
+    , xWhite_svg : Asset
+    }
+
+
+assets : Assets
+assets =
+    { activity = ""
+    , arrowDown = ""
+    , attention_svg = Asset ""
+    , bulb = ""
+    , calendar = ""
+    , checkmark = ""
+    , class = ""
+    , clever = ""
+    , clock = ""
+    , commentNotStarred_png = Asset ""
+    , commentStarred_png = Asset ""
+    , compass = ""
+    , darkBlueCheckmark_svg = Asset ""
+    , diagnostic = ""
+    , document = ""
+    , download = ""
+    , edit = ""
+    , editWriting = ""
+    , exclamation = ""
+    , exclamationPoint_svg = Asset ""
+    , facebookBlue_svg = Asset ""
+    , flipper = ""
+    , footsteps = ""
+    , gear = ""
+    , hint_png = Asset ""
+    , iconCalendar_svg = Asset ""
+    , iconCheck_png = Asset ""
+    , iconFlag_png = Asset ""
+    , icons_arrowDownBlue_svg = Asset ""
+    , icons_arrowRightBlue_svg = Asset ""
+    , icons_clockRed_svg = Asset ""
+    , icons_equals_svg = Asset ""
+    , icons_helpBlue_svg = Asset ""
+    , icons_peerReview_svg = Asset ""
+    , icons_plusBlue_svg = Asset ""
+    , icons_quickWrite_svg = Asset ""
+    , icons_searchGray_svg = Asset ""
+    , icons_xBlue_svg = Asset ""
+    , key = ""
+    , leaderboard = ""
+    , leftArrowBlue_png = Asset ""
+    , level1Badge_png = Asset ""
+    , level2Badge_png = Asset ""
+    , level3Badge_png = Asset ""
+    , lock = ""
+    , logoRedBlack_svg = Asset ""
+    , newspaper = ""
+    , openClose = ""
+    , performance = ""
+    , personBlue_svg = Asset ""
+    , practice = ""
+    , premiumLock_svg = Asset ""
+    , preview = ""
+    , quiz = ""
+    , seemore = ""
+    , share = ""
+    , smallCheckmark_png = Asset ""
+    , sort = ""
+    , sortArrow = ""
+    , speedometer = ""
+    , squiggly_png = Asset ""
+    , startingOffBadge_png = Asset ""
+    , teach_assignments_copyWhite_svg = Asset ""
+    , twitterBlue_svg = Asset ""
+    , unarchiveBlue2x_png = Asset ""
+    , writingAssignment = ""
+    , writingcycle = ""
+    , x = ""
+    , xWhite_svg = Asset ""
+    }
--- styleguide-app/NriModules.elm
+++ styleguide-app/NriModules.elm
@@ -1,5 +1,6 @@
 module NriModules exposing (ModuleStates, Msg, init, nriThemedModules, styles, subscriptions, update)
 
+import Assets exposing (assets)
 import DEPRECATED.Css.File exposing (Stylesheet, compile, stylesheet)
 import Examples.Colors
 import Examples.Fonts
@@ -13,8 +14,8 @@ import Html.Attributes exposing (..)
 import ModuleExample exposing (Category(..), ModuleExample)
 import Navigation
 import Nri.Ui.AssetPath as AssetPath exposing (Asset(Asset))
-import Nri.Ui.Icon.V1
-import Nri.Ui.SegmentedControl.V3
+import Nri.Ui.Icon.V2
+import Nri.Ui.SegmentedControl.V4
 import Nri.Ui.Text.V1 as Text
 import Nri.Ui.TextArea.V1 as TextArea
 import String.Extra
@@ -92,8 +93,8 @@ container width children =
 
 nriThemedModules : ModuleStates -> List (ModuleExample Msg)
 nriThemedModules model =
-    [ Examples.Icon.example assets
-    , Examples.SegmentedControl.example assets SegmentedControlMsg model.segmentedControlState
+    [ Examples.Icon.example
+    , Examples.SegmentedControl.example SegmentedControlMsg model.segmentedControlState
     , Examples.Text.example
     , Examples.Text.Writing.example
     , Examples.Fonts.example
@@ -124,156 +125,8 @@ styles =
           [ ModuleExample.styles
           ]
         , (Examples.Icon.styles |> .css) ()
-        , (Nri.Ui.Icon.V1.styles |> .css) ()
-        , (Nri.Ui.SegmentedControl.V3.styles |> .css) ()
+        , (Nri.Ui.Icon.V2.styles |> .css) ()
+        , (Nri.Ui.SegmentedControl.V4.styles |> .css) ()
         , (Text.styles |> .css) ()
         , (TextArea.styles |> .css) assets
         ]
-
-
-type alias Assets =
-    { activity : String
-    , arrowDown : String
-    , attention_svg : Asset
-    , bulb : String
-    , calendar : String
-    , checkmark : String
-    , class : String
-    , clever : String
-    , clock : String
-    , commentNotStarred_png : Asset
-    , commentStarred_png : Asset
-    , compass : String
-    , darkBlueCheckmark_svg : Asset
-    , diagnostic : String
-    , document : String
-    , download : String
-    , edit : String
-    , editWriting : String
-    , exclamation : String
-    , exclamationPoint_svg : Asset
-    , facebookBlue_svg : Asset
-    , flipper : String
-    , footsteps : String
-    , gear : String
-    , hint_png : Asset
-    , iconCalendar_svg : Asset
-    , iconCheck_png : Asset
-    , iconFlag_png : Asset
-    , icons_arrowDownBlue_svg : Asset
-    , icons_arrowRightBlue_svg : Asset
-    , icons_clockRed_svg : Asset
-    , icons_equals_svg : Asset
-    , icons_helpBlue_svg : Asset
-    , icons_peerReview_svg : Asset
-    , icons_plusBlue_svg : Asset
-    , icons_quickWrite_svg : Asset
-    , icons_searchGray_svg : Asset
-    , icons_xBlue_svg : Asset
-    , icons_xBlue_svg : Asset
-    , key : String
-    , leaderboard : String
-    , leftArrowBlue_png : Asset
-    , level1Badge_png : Asset
-    , level2Badge_png : Asset
-    , level3Badge_png : Asset
-    , lock : String
-    , logoRedBlack_svg : Asset
-    , newspaper : String
-    , openClose : String
-    , performance : String
-    , personBlue_svg : Asset
-    , practice : String
-    , premiumLock_svg : Asset
-    , preview : String
-    , quiz : String
-    , seemore : String
-    , share : String
-    , smallCheckmark_png : Asset
-    , sort : String
-    , sortArrow : String
-    , speedometer : String
-    , squiggly_png : Asset
-    , startingOffBadge_png : Asset
-    , teach_assignments_copyWhite_svg : Asset
-    , twitterBlue_svg : Asset
-    , unarchiveBlue2x_png : Asset
-    , writingAssignment : String
-    , writingcycle : String
-    , x : String
-    , xWhite_svg : Asset
-    }
-
-
-assets : Assets
-assets =
-    { activity = ""
-    , arrowDown = ""
-    , attention_svg = Asset ""
-    , bulb = ""
-    , calendar = ""
-    , checkmark = ""
-    , class = ""
-    , clever = ""
-    , clock = ""
-    , commentNotStarred_png = Asset ""
-    , commentStarred_png = Asset ""
-    , compass = ""
-    , darkBlueCheckmark_svg = Asset ""
-    , diagnostic = ""
-    , document = ""
-    , download = ""
-    , edit = ""
-    , editWriting = ""
-    , exclamation = ""
-    , exclamationPoint_svg = Asset ""
-    , facebookBlue_svg = Asset ""
-    , flipper = ""
-    , footsteps = ""
-    , gear = ""
-    , hint_png = Asset ""
-    , iconCalendar_svg = Asset ""
-    , iconCheck_png = Asset ""
-    , iconFlag_png = Asset ""
-    , icons_arrowDownBlue_svg = Asset ""
-    , icons_arrowRightBlue_svg = Asset ""
-    , icons_clockRed_svg = Asset ""
-    , icons_equals_svg = Asset ""
-    , icons_helpBlue_svg = Asset ""
-    , icons_peerReview_svg = Asset ""
-    , icons_plusBlue_svg = Asset ""
-    , icons_quickWrite_svg = Asset ""
-    , icons_searchGray_svg = Asset ""
-    , icons_xBlue_svg = Asset ""
-    , key = ""
-    , leaderboard = ""
-    , leftArrowBlue_png = Asset ""
-    , level1Badge_png = Asset ""
-    , level2Badge_png = Asset ""
-    , level3Badge_png = Asset ""
-    , lock = ""
-    , logoRedBlack_svg = Asset ""
-    , newspaper = ""
-    , openClose = ""
-    , performance = ""
-    , personBlue_svg = Asset ""
-    , practice = ""
-    , premiumLock_svg = Asset ""
-    , preview = ""
-    , quiz = ""
-    , seemore = ""
-    , share = ""
-    , smallCheckmark_png = Asset ""
-    , sort = ""
-    , sortArrow = ""
-    , speedometer = ""
-    , squiggly_png = Asset ""
-    , startingOffBadge_png = Asset ""
-    , teach_assignments_copyWhite_svg = Asset ""
-    , twitterBlue_svg = Asset ""
-    , unarchiveBlue2x_png = Asset ""
-    , writingAssignment = ""
-    , writingcycle = ""
-    , x = ""
-    , xWhite_svg = Asset ""
-    }
