38ca396ac8c73b8a2d676e86ae23c95a931c01a1
Luke Westby <lukewestby1@gmail.com>
2018-06-11 11:08:49 -0700

    Updates to TextArea (#70)
    * update textarea js code and make it npm-ready
    
    * use an attribute instead so dom debugging is easier
    
    * use data- attribute to be I D I O M A T I C
    
    * version the custom element
    
    * include marica's resize logic improvements
    
    * changes to elm module
    
    * clean up styleguide build and use v3 in styleguide

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ We try to avoid breaking changes and the associated major version bumps in this 
 
 Suppose you just released version `5.0.1`, a small styling fix in the checkbox widget, for a story you're working on. If the project you're working in currently pulls in `noredink-ui` at version `4.x`, then getting to your styling fix means pulling in a new major version of `noredink-ui`. This breaks all `TextArea` widgets across the project, so those will need to be fixed before you can do anything else, potentially a big effort.
 
-To prevent these big Yaks from suddenly showing up in seemingly trivial tasks we prefer to avoid breaking changes in the package. Instead when we need to make a breaking change in a widget, we create a new module for it `Nri.Ui.MyWidget.VX`.
+To prevent these big Yaks from suddenly showing up in seemingly trivial tasks we prefer to avoid breaking changes in the package. Instead when we need to make a breaking change in a widget, we create a new module for it `Nri.Ui.MyWidget.VX`. Similarly, when we build custom elements in JavaScript we create a file `lib/MyWidget/VX.js` and define a custom element `nri-mywidget-vX`.
 
 We should change this process if we feel it's not working for us!
 
--- src/Nri/Ui/TextArea/V3.elm
+++ src/Nri/Ui/TextArea/V3.elm
@@ -88,61 +88,38 @@ contentCreation model =
 view_ : Theme -> Model msg -> Html msg
 view_ theme model =
     let
-        minHeight =
+        autoresizeAttrs =
             case model.height of
+                AutoResize _ ->
+                    [ Attributes.attribute "data-autoresize" "" ]
+
                 Fixed ->
                     []
-
-                AutoResize minimumHeight ->
-                    [ calculateMinHeight theme minimumHeight
-                        |> Css.minHeight
-                    ]
-
-        sharedAttributes =
-            [ Events.onInput model.onInput
-            , Attributes.id (generateId model.label)
-            , Attributes.css
-                [ InputStyles.input theme model.isInError
-                ]
-            , Attributes.autofocus model.autofocus
-            , Attributes.placeholder model.placeholder
-            , Attributes.attribute "data-gramm" "false" -- disables grammarly to prevent https://github.com/NoRedInk/NoRedInk/issues/14859
-            , Attributes.css
-                (minHeight
-                    ++ [ Css.boxSizing Css.borderBox ]
-                )
-            ]
     in
-    Html.div
-        [ Attributes.css [ Css.position Css.relative ]
-        ]
-        [ case model.height of
-            AutoResize _ ->
-                {- NOTES:
-                   The autoresize-textarea element is implemented to pass information applied to itself to an internal
-                   textarea element that it inserts into the DOM automatically. Maintaing this behavior may require some
-                   changes on your part, as listed below.
+    Html.styled Html.div
+        [ Css.position Css.relative ]
+        []
+        [ Html.styled (Html.node "nri-textarea-v3")
+            [ Css.display Css.block ]
+            autoresizeAttrs
+            [ Html.styled Html.textarea
+                [ InputStyles.input theme model.isInError
+                , Css.boxSizing Css.borderBox
+                , case model.height of
+                    AutoResize minimumHeight ->
+                        Css.minHeight (calculateMinHeight theme minimumHeight)
 
-                   - When adding an Html.Attribute that is a _property_, you must edit Nri/TextArea.js to ensure that a getter and setter
-                   are set up to properly reflect the property to the actual textarea element that autoresize-textarea creates
-                   - When adding a new listener from Html.Events, you must edit Nri/TextArea.js to ensure that a listener is set up on
-                   the textarea that will trigger this event on the autoresize-textarea element itself. See AutoresizeTextArea.prototype._onInput
-                   and AutoresizeTextArea.prototype.connectedCallback for an example pertaining to the `input` event
-                   - When adding a new Html.Attribute that is an _attribute_, you don't have to do anything. All attributes are
-                   automatically reflected onto the textarea element via AutoresizeTextArea.prototype.attributeChangedCallback
-                -}
-                Html.node "autoresize-textarea"
-                    (sharedAttributes
-                        ++ [ -- setting the default value via a text node doesn't play well with the custom element,
-                             -- but we'll be able to switch to the regular value property in 0.19 anyway
-                             Attributes.defaultValue model.value
-                           ]
-                    )
-                    []
-
-            Fixed ->
-                Html.textarea sharedAttributes
-                    [ Html.text model.value ]
+                    Fixed ->
+                        Css.batch []
+                ]
+                [ Events.onInput model.onInput
+                , Attributes.id (generateId model.label)
+                , Attributes.autofocus model.autofocus
+                , Attributes.placeholder model.placeholder
+                , Attributes.attribute "data-gramm" "false" -- disables grammarly to prevent https://github.com/NoRedInk/NoRedInk/issues/14859
+                ]
+                [ Html.text model.value ]
+            ]
         , if not model.showLabel then
             Html.label
                 [ Attributes.for (generateId model.label)
