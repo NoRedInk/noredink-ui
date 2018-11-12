ec6f90884e01a32b142f41c635568c1124f4ee06
Luke Westby <lukewestby1@gmail.com>
2018-11-12 14:08:53 -0800

    update scripts

--- scripts/check-exposed.py
+++ scripts/check-exposed.py
@@ -43,23 +43,27 @@ def read_module(filename):
         return re_module.search(elm.read()).group("module")
 
 
-def find_v_modules():
-    for dirname, dirnames, filenames in os.walk("src/Nri"):
+def find_v_modules(root):
+    for dirname, dirnames, filenames in os.walk(root + "/Nri"):
         filenames = filter(p_v_file, filenames)
         filepaths = (os.path.join(dirname, filename) for filename in filenames)
         yield from map(read_module, filepaths)
 
 
-def read_exposed_modules():
-    with open("elm-package.json", "r", encoding="utf-8") as pkg:
+def read_exposed_modules(path):
+    with open(path, "r", encoding="utf-8") as pkg:
         yield from json.load(pkg).get("exposed-modules")
 
 
 if __name__ == "__main__":
-    available = set(find_v_modules())
-    exposed = set(read_exposed_modules())
+    available_18 = set(find_v_modules("src-0.18"))
+    available_19 = set(find_v_modules("src"))
+    exposed_18 = set(read_exposed_modules("elm-package.json"))
+    exposed_19 = set(read_exposed_modules("elm.json"))
     # XXX: Do we need to check that parent modules are exposed too?
-    missing = available - exposed
+    missing_18 = available_18 - exposed_18
+    missing_19 = available_19 - exposed_19
+    missing = missing_18.union(missing_19)
     for module in sorted(missing):
         print("Not exposed:", module, file=sys.stderr)
     raise SystemExit(
