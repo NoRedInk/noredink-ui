9c30f2de42561ddf1718a21fa794cf736c07be6a
Tessa Kelly <tessakkelly@gmail.com>
2019-03-21 14:16:51 -0700

    Pull the icon styles over from the monolith

--- src/Nri/Ui/BannerAlert/V3.elm
+++ src/Nri/Ui/BannerAlert/V3.elm
@@ -15,9 +15,11 @@ module Nri.Ui.BannerAlert.V3 exposing
 import Accessibility.Styled as Html exposing (Html)
 import Css
 import Css.Global
-import Html.Styled.Attributes exposing (css)
+import Html.Styled.Attributes as Attributes exposing (css)
+import Nri.Ui.AssetPath as AssetPath exposing (Asset(..))
 import Nri.Ui.Colors.V1 as Colors
 import Nri.Ui.Fonts.V1
+import Nri.Ui.Icon.V4 as Icon exposing (IconType)
 
 
 {-| A banner to show error alerts
@@ -78,6 +80,46 @@ banner { color, backgroundColor } alertMessage =
         [ notification alertMessage ]
 
 
+type BannerType
+    = Error -- exclamationPoint_svg
+    | Neutral -- tip_svg
+    | Success --  checkWhite_svg
+
+
+icon :
+    { r | exclamationPoint_svg : Asset, tip_svg : Asset, checkWhite_svg : Asset }
+    -> BannerType
+    -> Html msg
+icon assets bannerType =
+    let
+        ( containerStyle, iconStyle, asset ) =
+            case bannerType of
+                Error ->
+                    ( errorIcon
+                    , errorIconImage
+                    , assets.exclamationPoint_svg
+                    )
+
+                Neutral ->
+                    ( neutralIcon
+                    , neutralIconImage
+                    , assets.tip_svg
+                    )
+
+                Success ->
+                    ( successIcon
+                    , successIconImage
+                    , assets.checkWhite_svg
+                    )
+    in
+    Html.div [ css containerStyle ]
+        [ Html.decorativeImg
+            [ css iconStyle
+            , Attributes.src (AssetPath.url asset)
+            ]
+        ]
+
+
 notification : String -> Html msg
 notification message =
     Html.div
@@ -90,3 +132,59 @@ notification message =
             ]
         ]
         [ Html.text message ]
+
+
+
+-- ICON STYLES
+
+
+errorIcon : List Css.Style
+errorIcon =
+    [ iconContainer
+    , Css.backgroundColor Colors.purple
+    ]
+
+
+errorIconImage : List Css.Style
+errorIconImage =
+    [ Css.height (Css.px 25)
+    ]
+
+
+neutralIcon : List Css.Style
+neutralIcon =
+    [ iconContainer
+    ]
+
+
+neutralIconImage : List Css.Style
+neutralIconImage =
+    [ Css.height (Css.px 50)
+    ]
+
+
+successIcon : List Css.Style
+successIcon =
+    [ iconContainer
+    , Css.backgroundColor Colors.green
+    ]
+
+
+successIconImage : List Css.Style
+successIconImage =
+    [ Css.height (Css.px 20)
+    ]
+
+
+iconContainer : Css.Style
+iconContainer =
+    Css.batch
+        [ Css.borderRadius (Css.pct 50)
+        , Css.displayFlex
+        , Css.alignItems Css.center
+        , Css.justifyContent Css.center
+        , Css.width (Css.px 50)
+        , Css.height (Css.px 50)
+        , Css.marginRight (Css.px 20)
+        , Css.flexShrink (Css.num 0)
+        ]
