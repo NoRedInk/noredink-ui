6a459f503d9ce9d8fcdf3e8cec21380053a14545
Tessa Kelly <tessakkelly@gmail.com>
2019-05-07 11:29:48 -0700

    Inline the styles

--- src/Nri/Ui/DisclosureIndicator/V2.elm
+++ src/Nri/Ui/DisclosureIndicator/V2.elm
@@ -26,8 +26,21 @@ type alias Assets r =
 
 {-| -}
 view : Assets a -> Config -> Html msg
-view =
-    viewWithStyle headerStyle
+view assets config =
+    viewWithStyle
+        [ marginRight (px 10)
+        , width (px 15)
+        , height (px 15)
+        , cursor pointer
+        , property "transition" "transform 0.2s"
+        , if config.isOpen then
+            transform (rotate <| deg 0)
+
+          else
+            transform (rotate <| deg -90)
+        ]
+        assets
+        config
 
 
 {-| The inline variant of the indicator is smaller and occupies
@@ -36,11 +49,23 @@ without breaking text flow. Also, it rotates from right to
 down direction when expanding.
 -}
 viewInline : Assets a -> Config -> Html msg
-viewInline =
-    viewWithStyle inlineStyle
+viewInline assets config =
+    viewWithStyle
+        [ padding2 (px 0) (px 8)
+        , height (px 9)
+        , cursor pointer
+        , property "transition" "transform 0.1s"
+        , if config.isOpen then
+            transform (rotate <| deg 0)
+
+          else
+            transform (rotate <| deg -90)
+        ]
+        assets
+        config
 
 
-viewWithStyle : (Bool -> Css.Style) -> Assets a -> Config -> Html msg
+viewWithStyle : List Css.Style -> Assets a -> Config -> Html msg
 viewWithStyle style assets config =
     let
         label =
@@ -53,37 +78,6 @@ viewWithStyle style assets config =
     img
         [ alt label
         , Attributes.src <| AssetPath.url <| assets.icons_arrowDownBlue_svg
-        , Attributes.css [ style config.isOpen ]
+        , Attributes.css style
         ]
         []
-
-
-headerStyle : Bool -> Css.Style
-headerStyle isOpen =
-    Css.batch
-        [ marginRight (px 10)
-        , width (px 15)
-        , height (px 15)
-        , cursor pointer
-        , property "transition" "transform 0.2s"
-        , if isOpen then
-            transform (rotate <| deg 0)
-
-          else
-            transform (rotate <| deg -90)
-        ]
-
-
-inlineStyle : Bool -> Css.Style
-inlineStyle isOpen =
-    Css.batch
-        [ padding2 (px 0) (px 8)
-        , height (px 9)
-        , cursor pointer
-        , property "transition" "transform 0.1s"
-        , if isOpen then
-            transform (rotate <| deg 0)
-
-          else
-            transform (rotate <| deg -90)
-        ]
