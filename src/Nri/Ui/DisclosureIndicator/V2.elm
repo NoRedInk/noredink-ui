516200238bfbfdfa3c864de26b428f33c4952cf0
Tessa Kelly <tessakkelly@gmail.com>
2019-05-07 11:38:35 -0700

    :art: emphasize the differences between the styles

--- src/Nri/Ui/DisclosureIndicator/V2.elm
+++ src/Nri/Ui/DisclosureIndicator/V2.elm
@@ -30,15 +30,9 @@ view assets config =
         [ marginRight (px 10)
         , width (px 15)
         , height (px 15)
-        , cursor pointer
-        , property "transition" "transform 0.2s"
-        , if config.isOpen then
-            transform (rotate <| deg 0)
-
-          else
-            transform (rotate <| deg -90)
         ]
         assets
+        config
 
 
 {-| The inline variant of the indicator is smaller and occupies
@@ -51,21 +45,25 @@ viewInline assets config =
     viewWithStyle
         [ padding2 (px 0) (px 8)
         , height (px 9)
-        , cursor pointer
-        , property "transition" "transform 0.1s"
-        , if config.isOpen then
-            transform (rotate <| deg 0)
-
-          else
-            transform (rotate <| deg -90)
         ]
         assets
+        config
 
 
-viewWithStyle : List Css.Style -> Assets a -> Html msg
-viewWithStyle style assets =
+viewWithStyle : List Css.Style -> Assets a -> Config -> Html msg
+viewWithStyle style assets config =
     img
         [ Attributes.src <| AssetPath.url <| assets.icons_arrowDownBlue_svg
-        , Attributes.css style
+        , Attributes.css
+            (style
+                ++ [ cursor pointer
+                   , property "transition" "transform 0.1s"
+                   , if config.isOpen then
+                        transform (rotate <| deg 0)
+
+                     else
+                        transform (rotate <| deg -90)
+                   ]
+            )
         ]
         []
