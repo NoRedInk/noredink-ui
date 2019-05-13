f0f2dd97e39371dd58d4a62c4154228fa32328a7
Katie Hughes <katie@noredink.com>
2019-05-13 16:58:34 -0700

    add tests for error page recovery text

---
+++ tests/Spec/Nri/Ui/Page/V3.elm
@@ -0,0 +1,50 @@
+module Spec.Nri.Ui.Page.V3 exposing (all)
+
+import Expect exposing (Expectation)
+import Html.Styled as Html
+import Nri.Ui.Page.V3 as Page
+import Test exposing (..)
+import Test.Html.Query as Query
+import Test.Html.Selector exposing (..)
+
+
+type Msg
+    = NoOp
+
+
+all : Test
+all =
+    describe "Nri.Ui.Page.V3"
+        [ describe "button text"
+            [ test "handles recovery text for ReturnTo" <|
+                \() ->
+                    Page.notFound
+                        { link = NoOp
+                        , recoveryText = Page.ReturnTo "the main page"
+                        }
+                        |> Html.toUnstyled
+                        |> Query.fromHtml
+                        |> Expect.all
+                            [ Query.has [ text "Return to the main page" ] ]
+            , test "handles recovery text for Reload" <|
+                \() ->
+                    Page.notFound
+                        { link = NoOp
+                        , recoveryText = Page.Reload
+                        }
+                        |> Html.toUnstyled
+                        |> Query.fromHtml
+                        |> Expect.all
+                            [ Query.has [ text "Reload the page" ] ]
+            , test "handles recovery text for Custom" <|
+                \() ->
+                    Page.notFound
+                        { link = NoOp
+                        , recoveryText = Page.Custom "cats"
+                        }
+                        |> Html.toUnstyled
+                        |> Query.fromHtml
+                        |> Expect.all
+                            [ Query.has [ text "cats" ] ]
+            ]
+        ]
