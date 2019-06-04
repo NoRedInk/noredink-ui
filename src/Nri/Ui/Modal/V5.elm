78ca96686ecbedf2fa77f539d12c5d3db2622829
Tessa Kelly <tessakkelly@gmail.com>
2019-06-03 17:50:21 -0700

    Adds label

--- src/Nri/Ui/Modal/V5.elm
+++ src/Nri/Ui/Modal/V5.elm
@@ -183,7 +183,7 @@ closeButton msg =
         , Css.hover [ Css.color Colors.azureDark ]
         , Css.property "transition" "color 0.1s"
         ]
-        [ onClick msg ]
+        [ onClick msg, Widget.label "Close modal" ]
         [ Nri.Ui.Svg.V1.toHtml Nri.Ui.SpriteSheet.xSvg
         ]
 
