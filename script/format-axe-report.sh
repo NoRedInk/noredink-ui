4268f86f12b89dcc086a7d4cba64f8e2bda952a7
Brian Hicks <brian@brianthicks.com>
2019-07-23 17:10:08 -0500

    add more useful debugging steps

--- script/format-axe-report.sh
+++ script/format-axe-report.sh
@@ -20,4 +20,12 @@ if test "$NUM_ERRORS" -ne "$TARGET_ERRORS"; then
     echo
     echo 'If it went down, hooray!'
     echo "Check out ${0:-} and change the count to the reported value above."
+    echo
+    echo "Did it go up? That's OK too."
+    echo "Since there are so many errors right now, a decent debugging strategy is:"
+    echo
+    echo " 1. save tests/axe-report.log somewhere ('mv tests/axe-report.log tests/axe-report.log.failing' is one way)"
+    echo " 2. undo your changes ('git stash' or 'checkout master')"
+    echo " 3. regenerate the log with 'make tests/axe-report.log'"
+    echo " 4. compare the output with 'diff -u tests/axe-report.log tests/axe-report.log.failing'"
 fi
