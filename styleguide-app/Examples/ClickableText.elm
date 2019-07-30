0254b35bb2f6517811196a108b1b83a8909a7461
Tessa Kelly <tessakkelly@gmail.com>
2019-07-30 11:47:25 -0700

    cp V2 to V3

--- styleguide-app/Examples/ClickableText.elm
+++ styleguide-app/Examples/ClickableText.elm
@@ -11,7 +11,7 @@ import Debug.Control as Control exposing (Control)
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (css, id)
 import ModuleExample as ModuleExample exposing (Category(..), ModuleExample, ModuleMessages)
-import Nri.Ui.ClickableText.V2 as ClickableText exposing (Size(..))
+import Nri.Ui.ClickableText.V3 as ClickableText exposing (Size(..))
 import Nri.Ui.Icon.V5 as Icon
 import Nri.Ui.Svg.V1 as NriSvg exposing (Svg)
 import Nri.Ui.Text.V4 as Text
@@ -37,7 +37,7 @@ example unnamedMessages state =
         messages =
             unnamedMessages "ClickableTextExample"
     in
-    { name = "Nri.Ui.ClickableText.V2"
+    { name = "Nri.Ui.ClickableText.V3"
     , category = Buttons
     , content =
         [ viewExamples messages state ]
