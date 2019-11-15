f98f4ff745da84dcdf7db8436783cb5d3d50ec2f
Matt Charlton <Matt.charlton@noredink.com>
2019-11-15 11:49:45 +0000

    remove console.log

--- script/percy-tests.js
+++ script/percy-tests.js
@@ -1,7 +1,6 @@
 const PercyScript = require('@percy/script')
 
 PercyScript.run(async (page, percySnapshot) => {
-  console.log('Debug: does this run?')
   await page.goto('http://localhost:8000/');
   // ensure the page has loaded before capturing a snapshot
   await page.waitFor('.module-example__Nri-Ui-Alert-V4');
