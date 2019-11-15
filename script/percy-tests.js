056a67ba9c542419ac1e194b386035834b81c068
Matt Charlton <Matt.charlton@noredink.com>
2019-11-15 14:06:58 +0000

    extra pass of percySnapshot

--- script/percy-tests.js
+++ script/percy-tests.js
@@ -5,7 +5,7 @@ PercyScript.run(async (page, percySnapshot) => {
   // ensure the page has loaded before capturing a snapshot
   await page.waitFor('.module-example__Nri-Ui-Alert-V4')
 
-  await page.evaluate (() => {
+  await page.evaluate ((percySnapshot) => {
     let categories = Array.from(document.querySelectorAll('#categories a'))
     for (let category of categories)
       category.click()
