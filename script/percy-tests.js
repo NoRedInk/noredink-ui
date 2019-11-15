4457940f48b00dbaca1b8b8a238101b02f616397
Matt Charlton <Matt.charlton@noredink.com>
2019-11-15 16:00:31 +0000

    check each page individually

--- script/percy-tests.js
+++ script/percy-tests.js
@@ -7,4 +7,39 @@ PercyScript.run(async (page, percySnapshot) => {
   await page.waitFor('#categories')
 
   await percySnapshot('NoRedInk-UI', {widths: [1280]})
+  await page.goto('http://localhost:8000/#category/Animations')
+  await percySnapshot('Animations')
+
+  await page.goto('http://localhost:8000/#category/Buttons')
+  await percySnapshot('Buttons')
+
+  await page.goto('http://localhost:8000/#category/Colors')
+  await percySnapshot('Colors')
+
+  await page.goto('http://localhost:8000/#category/Icons')
+  await percySnapshot('Icons')
+
+  await page.goto('http://localhost:8000/#category/Inputs')
+  await percySnapshot('Inputs Unchecked')
+
+  await page.goto('http://localhost:8000/#category/Layout')
+  await percySnapshot('Layout')
+
+  await page.goto('http://localhost:8000/#category/Modals')
+  await percySnapshot('Modals')
+
+  await page.goto('http://localhost:8000/#category/Pages')
+  await percySnapshot('Pages')
+
+  await page.goto('http://localhost:8000/#category/Tables')
+  await percySnapshot('Tables')
+
+  await page.goto('http://localhost:8000/#category/Text')
+  await percySnapshot('Text')
+
+  await page.goto('http://localhost:8000/#category/Widgets')
+  await percySnapshot('Widgets')
+
+  await page.goto('http://localhost:8000/#category/Messaging')
+  await percySnapshot('Messaging')
 })
