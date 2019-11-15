aacfd158e10c9c7fe30b29540c2a074e90a47667
Matt Charlton <Matt.charlton@noredink.com>
2019-11-15 13:37:27 +0000

    capture each page individually

--- script/percy-tests.js
+++ script/percy-tests.js
@@ -1,8 +1,14 @@
 const PercyScript = require('@percy/script')
 
 PercyScript.run(async (page, percySnapshot) => {
-  await page.goto('http://localhost:8000/');
+  await page.goto('http://localhost:8000/')
   // ensure the page has loaded before capturing a snapshot
-  await page.waitFor('.module-example__Nri-Ui-Alert-V4');
-  await percySnapshot('homepage');
+  await page.waitFor('.module-example__Nri-Ui-Alert-V4')
+
+  const categories = Array.from(document.querySelectorAll('#categories a'))
+
+  categories.forEach((category) => {
+    await page.click(category)
+    await percySnapshot(category.innerText)
+  })
 })
