fbe230d570f9ce319907adc9b5c5b77b4cc7592e
Matt Charlton <Matt.charlton@noredink.com>
2019-11-15 13:49:36 +0000

    wait till page loads

--- script/percy-tests.js
+++ script/percy-tests.js
@@ -3,12 +3,11 @@ const PercyScript = require('@percy/script')
 PercyScript.run(async (page, percySnapshot) => {
   await page.goto('http://localhost:8000/')
   // ensure the page has loaded before capturing a snapshot
-  await page.waitFor('.module-example__Nri-Ui-Alert-V4')
-
-  const categories = Array.from(window.document.querySelectorAll('#categories a'))
-
-  categories.forEach(async (category) => {
-    await page.click(category)
-    await percySnapshot(category.innerText)
+  await page.waitFor('.module-example__Nri-Ui-Alert-V4').then(() => {
+    const categories = Array.from(window.document.querySelectorAll('#categories a'))
+    categories.forEach(async (category) => {
+      await page.click(category)
+      await percySnapshot(category.innerText)
+    })
   })
 })
