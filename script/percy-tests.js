97a5c6c6b39f31e91fe110a5f8a3df0065e41dc8
Matt Charlton <Matt.charlton@noredink.com>
2019-11-15 11:23:31 +0000

    update selector

--- script/percy-tests.js
+++ script/percy-tests.js
@@ -3,6 +3,6 @@ const PercyScript = require('@percy/script')
 PercyScript.run(async (page, percySnapshot) => {
   await page.goto('http://localhost:8000/');
   // ensure the page has loaded before capturing a snapshot
-  await page.waitFor('#Nri-Ui-TextInput-Writing-onBlur-demonstration!');
+  await page.waitFor('.module-example__Nri-Ui-Alert-V4');
   await percySnapshot('homepage');
 })
