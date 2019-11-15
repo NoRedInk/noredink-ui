9187c060373e9569630dbc8cdc53fc9141877b70
Matt Charlton <Matt.charlton@noredink.com>
2019-11-15 13:58:00 +0000

    use page.evaluate()

--- script/percy-tests.js
+++ script/percy-tests.js
@@ -3,11 +3,12 @@ const PercyScript = require('@percy/script')
 PercyScript.run(async (page, percySnapshot) => {
   await page.goto('http://localhost:8000/')
   // ensure the page has loaded before capturing a snapshot
-  await page.waitFor('.module-example__Nri-Ui-Alert-V4').then(() => {
-    const categories = Array.from(window.document.querySelectorAll('#categories a'))
-    categories.forEach(async (category) => {
-      await page.click(category)
+  await page.waitFor('.module-example__Nri-Ui-Alert-V4'))
+
+  await page.evaluate (() => {
+    let categories = Array.from(document.querySelectorAll('#categories a'))
+    for (let category of categories)
+      category.click()
       await percySnapshot(category.innerText)
-    })
   })
 })
