a8f71059e715972acfbf3ceb2af5c99dc650a06c
Matt Charlton <Matt.charlton@noredink.com>
2019-11-15 15:59:49 +0000

    use a static id to check we've loaded

--- script/percy-tests.js
+++ script/percy-tests.js
@@ -4,6 +4,7 @@ PercyScript.run(async (page, percySnapshot) => {
   await page.goto('http://localhost:8000/')
   // ensure the page has loaded before capturing a snapshot
   await page.waitFor('.module-example__Nri-Ui-Alert-V4')
+  await page.waitFor('#categories')
 
   await percySnapshot('NoRedInk-UI', {widths: [1280]})
 })
