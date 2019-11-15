e28076a31e2c662316cf36fc83e327e763f6f0e6
Matt Charlton <Matt.charlton@noredink.com>
2019-11-15 14:00:22 +0000

    remove extra bracket

--- script/percy-tests.js
+++ script/percy-tests.js
@@ -3,7 +3,7 @@ const PercyScript = require('@percy/script')
 PercyScript.run(async (page, percySnapshot) => {
   await page.goto('http://localhost:8000/')
   // ensure the page has loaded before capturing a snapshot
-  await page.waitFor('.module-example__Nri-Ui-Alert-V4'))
+  await page.waitFor('.module-example__Nri-Ui-Alert-V4')
 
   await page.evaluate (() => {
     let categories = Array.from(document.querySelectorAll('#categories a'))
