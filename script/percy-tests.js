078912a2e1c3c4e0455a92c5cd9ff6d26b86ba18
Matt Charlton <Matt.charlton@noredink.com>
2019-11-19 10:49:23 +0000

    check for correct class before each capture

--- script/percy-tests.js
+++ script/percy-tests.js
@@ -6,24 +6,31 @@ PercyScript.run(async (page, percySnapshot) => {
   await page.waitFor('#categories')
 
   await page.goto('http://localhost:8000/#category/Animations')
+  await page.waitFor('.Animations')
   await percySnapshot('Animations')
 
   await page.goto('http://localhost:8000/#category/Buttons')
+  await page.waitFor('.Buttons')
   await percySnapshot('Buttons')
 
   await page.goto('http://localhost:8000/#category/Colors')
+  await page.waitFor('.Colors')
   await percySnapshot('Colors')
 
   await page.goto('http://localhost:8000/#category/Icons')
+  await page.waitFor('.Icons')
   await percySnapshot('Icons')
 
   await page.goto('http://localhost:8000/#category/Inputs')
+  await page.waitFor('.Inputs')
   await percySnapshot('Inputs')
 
   await page.goto('http://localhost:8000/#category/Layout')
+  await page.waitFor('.Layout')
   await percySnapshot('Layout')
 
   await page.goto('http://localhost:8000/#category/Modals')
+  await page.waitFor('.Modals')
   await percySnapshot('Modals')
   await page.click('#launch-info-modal')
   await percySnapshot('Full Info Modal')
@@ -33,17 +40,22 @@ PercyScript.run(async (page, percySnapshot) => {
   await page.click('[aria-label="Close modal"]')
 
   await page.goto('http://localhost:8000/#category/Pages')
+  await page.waitFor('.Error')
   await percySnapshot('Pages')
 
   await page.goto('http://localhost:8000/#category/Tables')
+  await page.waitFor('.Tables')
   await percySnapshot('Tables')
 
   await page.goto('http://localhost:8000/#category/Text')
+  await page.waitFor('.Text')
   await percySnapshot('Text')
 
   await page.goto('http://localhost:8000/#category/Widgets')
+  await page.waitFor('.Widgets')
   await percySnapshot('Widgets')
 
   await page.goto('http://localhost:8000/#category/Messaging')
+  await page.waitFor('.Alerts')
   await percySnapshot('Messaging')
 })
