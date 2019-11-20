6a96bf2a6346dd90eae5d2006df0a7e1e07efa79
Matt Charlton <Matt.charlton@noredink.com>
2019-11-20 14:48:47 +0000

    update test to use IDs, remove unnecessary page load

--- script/percy-tests.js
+++ script/percy-tests.js
@@ -1,36 +1,32 @@
 const PercyScript = require('@percy/script')
 
 PercyScript.run(async (page, percySnapshot) => {
-  await page.goto('http://localhost:8000/')
-  // ensure the page has loaded before capturing a snapshot
-  await page.waitFor('#categories')
-
   await page.goto('http://localhost:8000/#category/Animations')
-  await page.waitFor('.Animations')
+  await page.waitFor('#animations')
   await percySnapshot('Animations')
 
   await page.goto('http://localhost:8000/#category/Buttons')
-  await page.waitFor('.Buttons')
+  await page.waitFor('#buttons-and-links')
   await percySnapshot('Buttons')
 
   await page.goto('http://localhost:8000/#category/Colors')
-  await page.waitFor('.Colors')
+  await page.waitFor('#colors')
   await percySnapshot('Colors')
 
   await page.goto('http://localhost:8000/#category/Icons')
-  await page.waitFor('.Icons')
+  await page.waitFor('#icons')
   await percySnapshot('Icons')
 
   await page.goto('http://localhost:8000/#category/Inputs')
-  await page.waitFor('.Inputs')
+  await page.waitFor('#inputs')
   await percySnapshot('Inputs')
 
   await page.goto('http://localhost:8000/#category/Layout')
-  await page.waitFor('.Layout')
+  await page.waitFor('#layout')
   await percySnapshot('Layout')
 
   await page.goto('http://localhost:8000/#category/Modals')
-  await page.waitFor('.Modals')
+  await page.waitFor('#modals')
   await percySnapshot('Modals')
   await page.click('#launch-info-modal')
   await page.waitFor('[role="dialog"]')
@@ -42,22 +38,22 @@ PercyScript.run(async (page, percySnapshot) => {
   await page.click('[aria-label="Close modal"]')
 
   await page.goto('http://localhost:8000/#category/Pages')
-  await page.waitFor('.Error')
+  await page.waitFor('#error-pages')
   await percySnapshot('Pages')
 
   await page.goto('http://localhost:8000/#category/Tables')
-  await page.waitFor('.Tables')
+  await page.waitFor('#tables')
   await percySnapshot('Tables')
 
   await page.goto('http://localhost:8000/#category/Text')
-  await page.waitFor('.Text')
+  await page.waitFor('#text-and-fonts')
   await percySnapshot('Text')
 
   await page.goto('http://localhost:8000/#category/Widgets')
-  await page.waitFor('.Widgets')
+  await page.waitFor('#widgets')
   await percySnapshot('Widgets')
 
   await page.goto('http://localhost:8000/#category/Messaging')
-  await page.waitFor('.Alerts')
+  await page.waitFor('#alerts-and-messages')
   await percySnapshot('Messaging')
 })
