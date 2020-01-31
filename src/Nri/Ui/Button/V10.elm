b552235173343c6416c29a7832ffabf58b44ada2
Tessa Kelly <tessakkelly@gmail.com>
2020-01-31 10:42:18 -0800

    Avoid importing html

--- src/Nri/Ui/Button/V10.elm
+++ src/Nri/Ui/Button/V10.elm
@@ -64,7 +64,6 @@ import AttributeExtras exposing (targetBlank)
 import Css exposing (Style)
 import Css.Global
 import EventExtras.Styled as EventExtras
-import Html as RootHtml
 import Html.Styled as Styled
 import Html.Styled.Attributes as Attributes
 import Html.Styled.Events as Events
@@ -619,7 +618,7 @@ delete config =
           Widget.label config.label
         ]
         [ Svg.svg [ Svg.Attributes.viewBox "0 0 25 25" ]
-            [ Svg.title [] [ RootHtml.text "Delete" ]
+            [ Svg.title [] [ Styled.toUnstyled (Styled.text "Delete") ]
             , Svg.path
                 [ Svg.Attributes.fill "#146aff" -- TODO: this should be azure, but css colors aren't extractable afaik
                 , Svg.Attributes.d "M1.067 6.015c-1.423-1.422-1.423-3.526 0-4.948 1.422-1.423 3.526-1.423 4.948 0l6.371 6.37 6.371-6.37c1.422-1.423 3.783-1.423 5.176 0 1.423 1.422 1.423 3.782 0 5.176l-6.37 6.37 6.37 6.372c1.423 1.422 1.423 3.526 0 4.948-1.422 1.423-3.526 1.423-4.948 0l-6.371-6.37-6.371 6.37c-1.422 1.423-3.783 1.423-5.176 0-1.423-1.422-1.423-3.782 0-5.176l6.37-6.143-6.37-6.599z"
