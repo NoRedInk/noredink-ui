7e261af7aa0d226b9e5cea3ab9ec0bff09cb0065
Tessa Kelly <tessakkelly@gmail.com>
2020-02-18 10:45:59 -0800

    Pull the link content up where it might be reused

--- styleguide-app/Examples/Text.elm
+++ styleguide-app/Examples/Text.elm
@@ -21,23 +21,30 @@ example =
     , content =
         let
             longerBody =
-                [ "Be on the lookout for a new and improved assignment"
+                [ " "
+                , "Be on the lookout for a new and improved assignment"
                 , "creation form! Soon, you'll be able to easily see a summary"
                 , "of the content you're assigning, as well as an estimate for"
                 , "how long the assignment will take."
+                , " "
                 ]
                     |> String.join " "
+
+            link =
+                Html.a
+                    [ Attributes.href "http://www.noredink.com"
+                    , Attributes.target "_blank"
+                    ]
+                    [ Html.text "The quick brown fox jumps over the lazy dog." ]
         in
         [ Text.caption [ Html.text "NOTE: When using these styles, please read the documentation in the Elm module about \"Understanding spacing\"" ]
         , Heading.h2 [ Heading.style Heading.Top ] [ Html.text "Paragraph styles" ]
-        , Text.mediumBody [ Html.text <| "This is a mediumBody. " ++ longerBody ]
         , Text.mediumBody
-            [ Html.text "This is a mediumBody with with a link. "
-            , Html.a
-                [ Attributes.href "http://www.noredink.com"
-                , Attributes.target "_blank"
-                ]
-                [ Html.text longerBody ]
+            [ Html.text "This is a "
+            , Html.strong [] [ Html.text "mediumBody" ]
+            , Html.text ". "
+            , link
+            , Html.text longerBody
             ]
         , Text.smallBody [ Html.text <| "This is a smallBody. " ++ longerBody ]
         , Text.smallBodyGray [ Html.text <| "This is a smallBodyGray. " ++ longerBody ]
