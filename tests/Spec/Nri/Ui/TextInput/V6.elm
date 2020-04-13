36ce3cb9b2336c8d8f108acef377f7587d1b4a37
Aaron VonderHaar <av@noredink.com>
2020-04-13 16:08:54 -0700

    Inline Spec.Nri.Ui.TextInput.V6.basic

--- tests/Spec/Nri/Ui/TextInput/V6.elm
+++ tests/Spec/Nri/Ui/TextInput/V6.elm
@@ -9,24 +9,20 @@ import Test.Html.Selector exposing (id, tag)
 
 all : Test
 all =
-    let
-        basic =
-            { label = "label"
-            , isInError = False
-            , onInput = identity
-            , onBlur = Nothing
-            , placeholder = "placeholder"
-            , value = "value"
-            , autofocus = False
-            , showLabel = False
-            , type_ = TextInput.text
-            }
-    in
     describe "Nri.Ui.TextInput.V6"
         [ test "it uses the same DOM id that generateId produces" <|
             \() ->
                 TextInput.view
-                    { basic | label = "myLabel" }
+                    { label = "myLabel"
+                    , isInError = False
+                    , onInput = identity
+                    , onBlur = Nothing
+                    , placeholder = "placeholder"
+                    , value = "value"
+                    , autofocus = False
+                    , showLabel = False
+                    , type_ = TextInput.text
+                    }
                     |> Html.Styled.toUnstyled
                     |> Query.fromHtml
                     |> Query.has
