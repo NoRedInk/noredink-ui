086d8f332102f5c73659c57fc1a8128ef983a571
Aaron VonderHaar <av@noredink.com>
2020-04-27 14:44:27 -0700

    Move Callout examples into Message examples

--- styleguide-app/Examples.elm
+++ styleguide-app/Examples.elm
@@ -4,7 +4,6 @@ import Example exposing (Example)
 import Examples.Accordion as Accordion
 import Examples.AssignmentIcon as AssignmentIcon
 import Examples.Button as Button
-import Examples.Callout as Callout
 import Examples.Checkbox as Checkbox
 import Examples.ClickableSvg as ClickableSvg
 import Examples.ClickableText as ClickableText
@@ -92,25 +91,6 @@ all =
             (\msg ->
                 case msg of
                     ButtonState childState ->
-                        Just childState
-
-                    _ ->
-                        Nothing
-            )
-    , Callout.example
-        |> Example.wrapMsg CalloutMsg
-            (\msg ->
-                case msg of
-                    CalloutMsg childMsg ->
-                        Just childMsg
-
-                    _ ->
-                        Nothing
-            )
-        |> Example.wrapState CalloutState
-            (\msg ->
-                case msg of
-                    CalloutState childState ->
                         Just childState
 
                     _ ->
@@ -712,7 +692,6 @@ type State
     = AccordionState Accordion.State
     | AssignmentIconState AssignmentIcon.State
     | ButtonState Button.State
-    | CalloutState Callout.State
     | CheckboxState Checkbox.State
     | ClickableSvgState ClickableSvg.State
     | ClickableTextState ClickableText.State
@@ -750,7 +729,6 @@ type Msg
     = AccordionMsg Accordion.Msg
     | AssignmentIconMsg AssignmentIcon.Msg
     | ButtonMsg Button.Msg
-    | CalloutMsg Callout.Msg
     | CheckboxMsg Checkbox.Msg
     | ClickableSvgMsg ClickableSvg.Msg
     | ClickableTextMsg ClickableText.Msg
--- styleguide-app/Examples/Message.elm
+++ styleguide-app/Examples/Message.elm
@@ -4,11 +4,11 @@ import Category exposing (Category(..))
 import Css
 import Example exposing (Example)
 import Html.Styled as Html exposing (Html, a, div, h3, pre, text)
-import Html.Styled.Attributes as Attributes
+import Html.Styled.Attributes as Attributes exposing (href, title)
 import Nri.Ui.Alert.V4 as Alert
 import Nri.Ui.BannerAlert.V6 as BannerAlert
+import Nri.Ui.Callout.V1 as Callout exposing (callout)
 import Nri.Ui.Colors.V1 as Colors
-import Nri.Ui.Fonts.V1 as Fonts
 import Nri.Ui.Heading.V2 as Heading
 import Nri.Ui.Pennant.V2 as Pennant
 import Nri.Ui.Svg.V1 as Svg
@@ -131,6 +131,41 @@ example =
                   Nothing
                   """
                 ]
+            , Html.hr [] []
+            , Heading.h3 [] [ text "Message.callout (deprecated; talk with your designer, but generally prefer Message.large, or consider adding Message.medium)" ]
+            , -- PLAIN
+              Html.h3 [] [ Html.text "Originally Designed Use Case" ]
+            , callout
+                [ Callout.label (Html.text "BETA")
+                , Callout.custom (title "beta warning")
+                ]
+                [ Html.text "This tab is still a work in progress; some of your student's information may be missing."
+                , Html.br [] []
+                , Html.text "To share your thoughts and help us improve the experience, "
+                , Html.a [ href "#" ] [ Html.text "click here" ]
+                , Html.text "."
+                ]
+
+            -- WITH SIDE TEXT
+            , Html.h3 [] [ Html.text "Without side text" ]
+            , callout
+                [ Callout.custom (title "no side text") ]
+                [ Html.text "I feel weird without my side text!" ]
+
+            -- WITH CSS CUSTOMIZATIONS
+            , Html.h3 [] [ Html.text "With CSS customizations" ]
+            , callout
+                [ Callout.containerCss [ Css.margin (Css.px 20) ]
+                , Callout.label (Html.text "HMMM")
+                , Callout.custom (title "margin")
+                ]
+                [ Html.text "My container styles are customized to add margin around the callout" ]
+            , callout
+                [ Callout.contentCss [ Css.textTransform Css.uppercase ]
+                , Callout.label (Html.text "WOW!")
+                , Callout.custom (title "yelling")
+                ]
+                [ Html.text "My content styles are customized to yell about everything" ]
             ]
     }
 
