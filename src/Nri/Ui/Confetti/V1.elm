b3a02654bc652d49635d02f34e4dfcf65b2d1866
Tessa Kelly <tessakkelly@gmail.com>
2020-05-15 10:56:46 -0700

    Reorganize confetti module to group by view, model, update sections

--- src/Nri/Ui/Confetti/V1.elm
+++ src/Nri/Ui/Confetti/V1.elm
@@ -1,16 +1,14 @@
 module Nri.Ui.Confetti.V1 exposing
     ( Model, init
-    , Msg, burst, update, updateCenter
     , view
-    , subscriptions
+    , Msg, burst, update, updateCenter, subscriptions
     )
 
 {-|
 
 @docs Model, init
-@docs Msg, burst, update, updateCenter
 @docs view
-@docs subscriptions
+@docs Msg, burst, update, updateCenter, subscriptions
 
 -}
 
@@ -25,7 +23,15 @@ import Random.Extra
 import Random.Float exposing (normal)
 
 
+
+-- MODEL
+
+
 {-| -}
+type Model
+    = System (ParticleSystem.System Confetti) Float
+
+
 type Confetti
     = Word
         { color : Color
@@ -40,21 +46,97 @@ type Confetti
 
 
 {-| -}
-type Model
-    = System (ParticleSystem.System Confetti) Float
-
-
-{-| -}
-type alias Msg =
-    ParticleSystem.Msg Confetti
-
-
-{-| -}
 init : Float -> Model
 init center =
     System
         (ParticleSystem.init (Random.initialSeed 0))
         center
+
+
+
+-- VIEW
+
+
+{-| -}
+view : Model -> Html.Html msg
+view (System system _) =
+    system
+        |> ParticleSystem.viewCustom viewConfetti
+            (Html.div
+                [ Attributes.style "position" "absolute"
+                , Attributes.style "top" "0"
+                , Attributes.style "left" "0"
+                , Attributes.style "width" "100%"
+                , Attributes.style "height" "100vh"
+                , Attributes.style "pointer-events" "none"
+                ]
+            )
+
+
+viewConfetti : Particle Confetti -> Html.Html msg
+viewConfetti particle =
+    let
+        lifetime =
+            Particle.lifetimePercent particle
+    in
+    case Particle.data particle of
+        Word { color, text, rotations } ->
+            Html.div
+                [ css
+                    [ Css.backgroundColor color
+                    , Css.padding (Css.px 4)
+                    , Css.borderRadius (Css.px 4)
+                    ]
+                , Attributes.style "position" "absolute"
+
+                {-
+                   There are important performance optimizations happening here:
+                       - `transform: translate / scale / rotate` are the most performant for animations
+                       - `will-change: transform` tells the browser our intent and it makes optimizations
+                          for an upcoming `transform`
+                       - Putting `rotate` after `translateX` and `translateY` is more performant
+                -}
+                , Attributes.style "left" "0px"
+                , Attributes.style "top" "0px"
+                , Attributes.style "transform-origin" "center"
+                , Attributes.style "will-change" "transform"
+                , Attributes.style "transform" <|
+                    "translateX("
+                        ++ String.fromFloat (Particle.leftPixels particle)
+                        ++ "px) translateY("
+                        ++ String.fromFloat (Particle.topPixels particle)
+                        ++ "px) rotate("
+                        ++ String.fromFloat ((rotations * lifetime) * 360)
+                        ++ "deg)"
+                ]
+                [ Html.text text ]
+
+        Square { color, rotationOffset, rotations } ->
+            Html.div
+                [ css
+                    [ Css.backgroundColor color
+                    , Css.width (Css.px 14)
+                    , Css.height (Css.px 14)
+                    ]
+                , Attributes.style "position" "absolute"
+                , Attributes.style "left" "0px"
+                , Attributes.style "top" "0px"
+                , Attributes.style "transform-origin" "center"
+                , Attributes.style "will-change" "transform"
+                , Attributes.style "transform" <|
+                    "translateX("
+                        ++ String.fromFloat (Particle.leftPixels particle)
+                        ++ "px) translateY("
+                        ++ String.fromFloat (Particle.topPixels particle)
+                        ++ "px) rotate("
+                        ++ String.fromFloat ((rotations * lifetime + rotationOffset) * 360)
+                        ++ "deg)"
+                ]
+                []
+
+
+
+-- UPDATE
 
 
 {-| -}
@@ -158,81 +240,8 @@ particleGenerator center generator =
 
 
 {-| -}
-view : Model -> Html.Html msg
-view (System system _) =
-    system
-        |> ParticleSystem.viewCustom viewConfetti
-            (Html.div
-                [ Attributes.style "position" "absolute"
-                , Attributes.style "top" "0"
-                , Attributes.style "left" "0"
-                , Attributes.style "width" "100%"
-                , Attributes.style "height" "100vh"
-                , Attributes.style "pointer-events" "none"
-                ]
-            )
-
-
-viewConfetti : Particle Confetti -> Html.Html msg
-viewConfetti particle =
-    let
-        lifetime =
-            Particle.lifetimePercent particle
-    in
-    case Particle.data particle of
-        Word { color, text, rotations } ->
-            Html.div
-                [ css
-                    [ Css.backgroundColor color
-                    , Css.padding (Css.px 4)
-                    , Css.borderRadius (Css.px 4)
-                    ]
-                , Attributes.style "position" "absolute"
-
-                {-
-                   There are important performance optimizations happening here:
-                       - `transform: translate / scale / rotate` are the most performant for animations
-                       - `will-change: transform` tells the browser our intent and it makes optimizations
-                          for an upcoming `transform`
-                       - Putting `rotate` after `translateX` and `translateY` is more performant
-                -}
-                , Attributes.style "left" "0px"
-                , Attributes.style "top" "0px"
-                , Attributes.style "transform-origin" "center"
-                , Attributes.style "will-change" "transform"
-                , Attributes.style "transform" <|
-                    "translateX("
-                        ++ String.fromFloat (Particle.leftPixels particle)
-                        ++ "px) translateY("
-                        ++ String.fromFloat (Particle.topPixels particle)
-                        ++ "px) rotate("
-                        ++ String.fromFloat ((rotations * lifetime) * 360)
-                        ++ "deg)"
-                ]
-                [ Html.text text ]
-
-        Square { color, rotationOffset, rotations } ->
-            Html.div
-                [ css
-                    [ Css.backgroundColor color
-                    , Css.width (Css.px 14)
-                    , Css.height (Css.px 14)
-                    ]
-                , Attributes.style "position" "absolute"
-                , Attributes.style "left" "0px"
-                , Attributes.style "top" "0px"
-                , Attributes.style "transform-origin" "center"
-                , Attributes.style "will-change" "transform"
-                , Attributes.style "transform" <|
-                    "translateX("
-                        ++ String.fromFloat (Particle.leftPixels particle)
-                        ++ "px) translateY("
-                        ++ String.fromFloat (Particle.topPixels particle)
-                        ++ "px) rotate("
-                        ++ String.fromFloat ((rotations * lifetime + rotationOffset) * 360)
-                        ++ "deg)"
-                ]
-                []
+type alias Msg =
+    ParticleSystem.Msg Confetti
 
 
 {-| -}
