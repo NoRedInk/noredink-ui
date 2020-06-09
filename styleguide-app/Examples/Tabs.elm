162989e54f99fd2f11c4e4e5a19124d95915b748
Tessa Kelly <tessakkelly@gmail.com>
2020-06-09 14:51:00 -0700

    Adds alignment demo to the example

--- styleguide-app/Examples/Tabs.elm
+++ styleguide-app/Examples/Tabs.elm
@@ -11,35 +11,66 @@ module Examples.Tabs exposing
 -}
 
 import Category exposing (Category(..))
+import Debug.Control as Control exposing (Control)
 import Example exposing (Example)
-import Html.Styled as Html
+import Html.Styled as Html exposing (Html, fromUnstyled)
 import List.Zipper
-import Nri.Ui.Tabs.V5 as Tabs
+import Nri.Ui.Tabs.V5 as Tabs exposing (Alignment(..))
 
 
-type State
+type alias State =
+    { selected : Id
+    , alignment : Control Alignment
+    }
+
+
+init : State
+init =
+    { selected = First
+    , alignment =
+        Control.choice
+            [ ( "Left", Control.value Left )
+            , ( "Center", Control.value Center )
+            , ( "Right", Control.value Right )
+            ]
+    }
+
+
+type Id
     = First
     | Second
 
 
-type alias Msg =
-    State
+type Msg
+    = SelectTab Id
+    | SetAlignment (Control Alignment)
+
+
+update : Msg -> State -> State
+update msg model =
+    case msg of
+        SelectTab id ->
+            { model | selected = id }
+
+        SetAlignment alignment ->
+            { model | alignment = alignment }
 
 
 example : Example State Msg
 example =
     { name = "Nri.Ui.Tabs.V5"
     , categories = [ Layout ]
-    , state = First
-    , update = \newTab oldTab -> ( newTab, Cmd.none )
+    , state = init
+    , update = \msg model -> ( update msg model, Cmd.none )
     , subscriptions = \_ -> Sub.none
     , view =
-        \tab ->
-            [ Tabs.view
+        \model ->
+            [ Control.view SetAlignment model.alignment |> fromUnstyled
+            , Tabs.view
                 { title = Nothing
-                , onSelect = identity
+                , onSelect = SelectTab
                 , tabs =
-                    case tab of
+                    case model.selected of
                         First ->
                             List.Zipper.from
                                 []
@@ -59,7 +90,7 @@ example =
 
                             Second ->
                                 Html.text "Second"
-                , alignment = Tabs.Center
+                , alignment = Control.currentValue model.alignment
                 }
             , Tabs.links
                 { title = Nothing
@@ -70,7 +101,7 @@ example =
                         []
                         (Tabs.NormalLink { label = "Nowhere", href = Nothing })
                         [ Tabs.NormalLink { label = "Elm", href = Just "http://elm-lang.org" }
-                        , Tabs.SpaLink { label = "Spa", href = "/#category/Layout", msg = Second }
+                        , Tabs.SpaLink { label = "Spa", href = "/#category/Layout", msg = SelectTab Second }
                         ]
                 }
             ]
