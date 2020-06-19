311ba32ec35dccef96e56d66a4b8e36ce0962157
Tessa Kelly <tessakkelly@gmail.com>
2020-06-19 14:42:28 -0700

    Adds keyboard shortcuts to all the examples

--- styleguide-app/KeyboardShortcuts.elm
+++ styleguide-app/KeyboardShortcuts.elm
@@ -23,11 +23,49 @@ type alias KeyboardShortcut =
 
 
 {-| -}
+view : List KeyboardShortcut -> Html msg
+view keyboardShortcuts =
+    case keyboardShortcuts of
+        [] ->
+            text ""
+
+        _ ->
+            details []
+                [ summary [] [ text "Keyboard Support" ]
+                , ul [] (List.map viewKeyboardActions keyboardShortcuts)
+                ]
+
+
+viewKeyboardActions : KeyboardShortcut -> Html msg
+viewKeyboardActions { keys, result } =
+    li []
+        [ strong [] [ text (String.join "+" (List.map keyToString keys)) ]
+        , text result
+        ]
+
+
+{-| -}
 type Key
     = Shift
     | Enter
     | Arrow Direction
     | Tab
+
+
+keyToString : Key -> String
+keyToString key =
+    case key of
+        Shift ->
+            "Shift"
+
+        Enter ->
+            "Enter"
+
+        Arrow direction ->
+            directionToString direction ++ " arrow"
+
+        Tab ->
+            "Tab"
 
 
 {-| -}
@@ -38,9 +76,17 @@ type Direction
     | Left
 
 
-{-| -}
-view : List KeyboardShortcut -> Html msg
-view keyboardShortcuts =
-    div []
-        [ text "TODO"
-        ]
+directionToString : Direction -> String
+directionToString direction =
+    case direction of
+        Up ->
+            "Up"
+
+        Right ->
+            "Right"
+
+        Down ->
+            "Down"
+
+        Left ->
+            "Left"
