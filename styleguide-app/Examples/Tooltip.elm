2a969a4b594798f0ccbc986e8950bb33e603ef05
Tessa Kelly <tessakkelly@gmail.com>
2020-09-03 10:26:48 -0700

    Re-order the tooltip example

--- styleguide-app/Examples/Tooltip.elm
+++ styleguide-app/Examples/Tooltip.elm
@@ -6,7 +6,7 @@ module Examples.Tooltip exposing (example, State, Msg)
 
 -}
 
-import Accessibility.Styled as Html
+import Accessibility.Styled as Html exposing (Html)
 import AtomicDesignType exposing (AtomicDesignType(..))
 import Category exposing (Category(..))
 import Css
@@ -18,14 +18,17 @@ import Nri.Ui.Text.V4 as Text
 import Nri.Ui.Tooltip.V2 as Tooltip
 
 
-type TooltipType
-    = PrimaryLabelOnClick
-    | PrimaryLabelOnHover
-    | AuxillaryDescription
-    | ToggleTipTop
-    | ToggleTipRight
-    | ToggleTipBottom
-    | ToggleTipLeft
+example : Example State Msg
+example =
+    { name = "Nri.Ui.Tooltip.V2"
+    , categories = [ Widgets ]
+    , atomicDesignType = Molecule
+    , keyboardSupport = []
+    , state = init
+    , update = update
+    , subscriptions = \_ -> Sub.none
+    , view = view
+    }
 
 
 type alias State =
@@ -35,7 +38,18 @@ type alias State =
 
 init : State
 init =
-    { openTooltip = Nothing }
+    { openTooltip = Nothing
+    }
+
+
+type TooltipType
+    = PrimaryLabelOnClick
+    | PrimaryLabelOnHover
+    | AuxillaryDescription
+    | ToggleTipTop
+    | ToggleTipRight
+    | ToggleTipBottom
+    | ToggleTipLeft
 
 
 type Msg
@@ -53,92 +67,82 @@ update msg model =
                 ( { model | openTooltip = Nothing }, Cmd.none )
 
 
-example : Example State Msg
-example =
-    { name = "Nri.Ui.Tooltip.V2"
-    , categories = [ Widgets ]
-    , atomicDesignType = Molecule
-    , keyboardSupport = []
-    , state = init
-    , update = update
-    , subscriptions = \_ -> Sub.none
-    , view =
-        \model ->
-            [ Text.mediumBody [ Html.text "These tooltips look similar, but serve different purposes when reading them via a screen-reader." ]
-            , Heading.h3 [] [ Html.text "primaryLabel" ]
-            , Text.smallBody
-                [ Html.text "A primary label is used when the tooltip content serves as the main label for its trigger content"
-                , Html.br []
-                , Html.text "e.g. when the trigger content is an icon with no text."
+view : State -> List (Html Msg)
+view model =
+    [ Text.mediumBody [ Html.text "These tooltips look similar, but serve different purposes when reading them via a screen-reader." ]
+    , Heading.h3 [] [ Html.text "primaryLabel" ]
+    , Text.smallBody
+        [ Html.text "A primary label is used when the tooltip content serves as the main label for its trigger content"
+        , Html.br []
+        , Html.text "e.g. when the trigger content is an icon with no text."
+        ]
+    , Tooltip.view
+        { triggerHtml = Html.text "Primary Label - OnClick Trigger"
+        , id = "primary label tooltip"
+        }
+        [ Tooltip.plaintext "Tooltip"
+        , Tooltip.primaryLabel
+        , Tooltip.onClick (ToggleTooltip PrimaryLabelOnClick)
+        , Tooltip.open (model.openTooltip == Just PrimaryLabelOnClick)
+        ]
+    , Html.br [ css [ Css.marginBottom (Css.px 20) ] ]
+    , Tooltip.view
+        { triggerHtml = Html.text "Primary Label - OnHover Trigger"
+        , id = "primary label tooltip"
+        }
+        [ Tooltip.plaintext "Tooltip"
+        , Tooltip.primaryLabel
+        , Tooltip.onHover (ToggleTooltip PrimaryLabelOnHover)
+        , Tooltip.open (model.openTooltip == Just PrimaryLabelOnHover)
+        ]
+    , Html.br [ css [ Css.marginBottom (Css.px 20) ] ]
+    , Heading.h3 [] [ Html.text "auxillaryDescription" ]
+    , Text.smallBody
+        [ Html.text "An auxillary description is used when the tooltip content provides supplementary information about its trigger content"
+        , Html.br []
+        , Html.text "e.g. when the trigger content is a word in the middle of a body of text that requires additional explanation."
+        ]
+    , Tooltip.view
+        { triggerHtml = Html.text "Auxillary Description Trigger"
+        , id = "Auxillary description"
+        }
+        [ Tooltip.plaintext "Tooltip"
+        , Tooltip.onClick (ToggleTooltip AuxillaryDescription)
+        , Tooltip.auxillaryDescription
+        , Tooltip.open (model.openTooltip == Just AuxillaryDescription)
+        ]
+    , Html.br [ css [ Css.marginBottom (Css.px 20) ] ]
+    , Heading.h3 [] [ Html.text "toggleTip" ]
+    , Text.smallBody [ Html.text "A Toggle Tip is triggered by the \"?\" icon and provides supplemental information for the page." ]
+    , Html.div [ css [ Css.displayFlex, Css.alignItems Css.center ] ]
+        [ Tooltip.toggleTip
+            { label = "More info"
+            }
+            [ Tooltip.html
+                [ Html.text "Tooltip On Top! "
+                , Html.a [ href "/" ] [ Html.text "Links work!" ]
                 ]
-            , Tooltip.view
-                { triggerHtml = Html.text "Primary Label - OnClick Trigger"
-                , id = "primary label tooltip"
-                }
-                [ Tooltip.plaintext "Tooltip"
-                , Tooltip.primaryLabel
-                , Tooltip.onClick (ToggleTooltip PrimaryLabelOnClick)
-                , Tooltip.open (model.openTooltip == Just PrimaryLabelOnClick)
-                ]
-            , Html.br [ css [ Css.marginBottom (Css.px 20) ] ]
-            , Tooltip.view
-                { triggerHtml = Html.text "Primary Label - OnHover Trigger"
-                , id = "primary label tooltip"
-                }
-                [ Tooltip.plaintext "Tooltip"
-                , Tooltip.primaryLabel
-                , Tooltip.onHover (ToggleTooltip PrimaryLabelOnHover)
-                , Tooltip.open (model.openTooltip == Just PrimaryLabelOnHover)
-                ]
-            , Html.br [ css [ Css.marginBottom (Css.px 20) ] ]
-            , Heading.h3 [] [ Html.text "auxillaryDescription" ]
-            , Text.smallBody
-                [ Html.text "An auxillary description is used when the tooltip content provides supplementary information about its trigger content"
-                , Html.br []
-                , Html.text "e.g. when the trigger content is a word in the middle of a body of text that requires additional explanation."
-                ]
-            , Tooltip.view
-                { triggerHtml = Html.text "Auxillary Description Trigger"
-                , id = "Auxillary description"
-                }
-                [ Tooltip.plaintext "Tooltip"
-                , Tooltip.onClick (ToggleTooltip AuxillaryDescription)
-                , Tooltip.auxillaryDescription
-                , Tooltip.open (model.openTooltip == Just AuxillaryDescription)
-                ]
-            , Html.br [ css [ Css.marginBottom (Css.px 20) ] ]
-            , Heading.h3 [] [ Html.text "toggleTip" ]
-            , Text.smallBody [ Html.text "A Toggle Tip is triggered by the \"?\" icon and provides supplemental information for the page." ]
-            , Html.div [ css [ Css.displayFlex, Css.alignItems Css.center ] ]
-                [ Tooltip.toggleTip
-                    { label = "More info"
-                    }
-                    [ Tooltip.html
-                        [ Html.text "Tooltip On Top! "
-                        , Html.a [ href "/" ] [ Html.text "Links work!" ]
-                        ]
-                    , Tooltip.onHover (ToggleTooltip ToggleTipTop)
-                    , Tooltip.open (model.openTooltip == Just ToggleTipTop)
-                    ]
-                , Text.mediumBody
-                    [ Html.text "This toggletip will open on top"
-                    ]
-                ]
-            , Html.div [ css [ Css.displayFlex, Css.alignItems Css.center ] ]
-                [ Tooltip.toggleTip
-                    { label = "More info"
-                    }
-                    [ Tooltip.html
-                        [ Html.text "Tooltip On Left! "
-                        , Html.a [ href "/" ] [ Html.text "Links work!" ]
-                        ]
-                    , Tooltip.onLeft
-                    , Tooltip.onHover (ToggleTooltip ToggleTipLeft)
-                    , Tooltip.open (model.openTooltip == Just ToggleTipLeft)
-                    ]
-                , Text.mediumBody
-                    [ Html.text "This toggletip will open on the left"
-                    ]
-                ]
+            , Tooltip.onHover (ToggleTooltip ToggleTipTop)
+            , Tooltip.open (model.openTooltip == Just ToggleTipTop)
             ]
-    }
+        , Text.mediumBody
+            [ Html.text "This toggletip will open on top"
+            ]
+        ]
+    , Html.div [ css [ Css.displayFlex, Css.alignItems Css.center ] ]
+        [ Tooltip.toggleTip
+            { label = "More info"
+            }
+            [ Tooltip.html
+                [ Html.text "Tooltip On Left! "
+                , Html.a [ href "/" ] [ Html.text "Links work!" ]
+                ]
+            , Tooltip.onLeft
+            , Tooltip.onHover (ToggleTooltip ToggleTipLeft)
+            , Tooltip.open (model.openTooltip == Just ToggleTipLeft)
+            ]
+        , Text.mediumBody
+            [ Html.text "This toggletip will open on the left"
+            ]
+        ]
+    ]
