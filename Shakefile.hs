4f270a498574d15b034c16e2de0c0f804364861d
Brian Hicks <brian@brianthicks.com>
2020-10-27 17:34:00 -0500

    port axe reports to shake

--- Shakefile.hs
+++ Shakefile.hs
@@ -9,3 +9,13 @@ main = shakeArgs shakeOptions {shakeFiles = "_build"} $ do
     need ["elm.json"]
     Stdout newConfig <- cmd "jq" "--indent" "4" ["{ root: \"../src\", tests: .[\"exposed-modules\"] }"] "elm.json"
     writeFileChanged out newConfig
+
+  "log/axe-report.json" %> \out -> do
+    need ["public", "script/run-axe.sh", "script/axe-puppeteer.js"]
+    Stdout report <- cmd "script/run-axe.sh"
+    writeFileChanged out report
+
+  "log/axe-report" %> \out -> do
+    need ["log/axe-report.json", "script/format-axe-report.sh", "script/axe-report.jq"]
+    Stdout report <- cmd "script/format-axe-report.sh" "log/axe-report.json"
+    writeFileChanged out report
