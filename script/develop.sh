ddf10872eda1ed8414729280554160a470892874
Juliano Solanho <juliano.solanho@gmail.com>
2020-11-05 15:27:24 -0300

    Reduce noise to repro bug

--- script/develop.sh
+++ script/develop.sh
@@ -1,37 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if test -d public; then
-    rm -rf public
-fi
-
-SHAKE_TARGET="${1:-public}"
-
-shake --compact "$SHAKE_TARGET"
-
-cat <<EOF
-== 👋 Hello! ==================================================================
-
-I'm watching files in styleguide-app and src for changes. If you make any
-changes, I'll try to be smart about what should change (things end up in the
-"public" directory if you want to check my work.) If you remove a file and it's
-still showing up, delete the "public" directory and restart me.
-
-To rebuild manually, hit SPC.
-
-To quit, hit "q" or ctrl-c.
-
-== thaaat's it from me! =======================================================
-
-EOF
-
-# start a web server in the background and tear it down when exiting
-./script/serve.sh public &
-SERVER_PID=$!
-cleanup() {
-    kill "$SERVER_PID"
-}
-trap cleanup EXIT INT
-
 # start a watcher. This loops forever, so we don't need to loop ourselves.
-git ls-files | entr -c -p shake --compact "$SHAKE_TARGET"
+git ls-files | entr shake --compact public
--- shell.nix
+++ shell.nix
@@ -9,31 +9,14 @@ stdenv.mkDerivation {
     # base dependencies
     git
     niv.niv
-    jq
 
     # building with Shake
-    haskellPackages.ormolu
     (haskellPackages.ghcWithPackages (ps: [ ps.shake ]))
-
-    # node dependencies
-    nodejs
-    nodePackages.npm
-
-    # elm dependencies
-    elmPackages.elm
-    elmPackages.elm-format
-    elmPackages.elm-test
-    elmPackages.elm-verify-examples
 
     # preview dependencies
     entr
-    python3
-
+    
     # stuff we need for running builds in a `nix-shell --pure` environment.
     which
   ] ++ lib.optionals stdenv.isLinux [ pkgs.fsatrace pkgs.strace pkgs.cacert ];
-
-  # environment variables
-  DEPRECATED_MODULES =
-    "Html,Accessibility,Accessibility.Aria,Accessibility.Key,Accessibility.Landmark,Accessibility.Live,Accessibility.Role,Accessibility.Style,Accessibility.Widget";
 }
