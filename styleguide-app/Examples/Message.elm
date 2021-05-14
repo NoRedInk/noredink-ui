f22342e8e5a59c54ca9d2ebdaa0f4a89ec6a0f0d
Tessa Kelly <tessakkelly@gmail.com>
2021-05-14 14:12:05 -0700

    Adjust the example to make it more clear what the debug controls actually control

--- styleguide-app/Examples/Message.elm
+++ styleguide-app/Examples/Message.elm
@@ -199,9 +199,10 @@ example =
                     else
                         text "Nice! The messages were dismissed. 👍"
             in
-            [ Control.view UpdateControl state.control
+            [ Heading.h3 [ Heading.css [ Css.marginBottom (Css.px 20) ] ]
+                [ text "Message.view" ]
+            , Control.view UpdateControl state.control
                 |> Html.fromUnstyled
-            , Heading.h3 [] [ text "Message.view" ]
             , orDismiss <|
                 Html.table [ css [ width (pct 100) ] ]
                     [ Html.tbody []
@@ -219,7 +220,14 @@ example =
                             ]
                         ]
                     ]
-            , Heading.h3 [] [ text "Message.somethingWentWrong" ]
+            , Heading.h3
+                [ Heading.css
+                    [ Css.marginTop (Css.px 20)
+                    , Css.borderTop3 (Css.px 2) Css.solid Colors.gray96
+                    , Css.paddingTop (Css.px 20)
+                    ]
+                ]
+                [ text "Message.somethingWentWrong" ]
             , Message.somethingWentWrong exampleRailsError
             ]
     }
