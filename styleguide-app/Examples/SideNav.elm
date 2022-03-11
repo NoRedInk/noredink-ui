ee0e46f06dc0411fbc3543e9a94c6760a5c2dfcb
Tessa Kelly <tessakkelly@gmail.com>
2022-03-10 16:36:59 -0800

    Start outlining the sidenav example

--- styleguide-app/Examples/SideNav.elm
+++ styleguide-app/Examples/SideNav.elm
@@ -11,6 +11,7 @@ import Category exposing (Category(..))
 import Css
 import Debug.Control as Control exposing (Control)
 import Debug.Control.Extra as ControlExtra
+import Debug.Control.View as ControlView
 import Example exposing (Example)
 import Html.Styled
 import Html.Styled.Attributes exposing (css)
@@ -31,9 +32,7 @@ example =
     , categories = [ Layout ]
     , keyboardSupport = []
     , preview = [ viewPreview ]
-    , view =
-        \state ->
-            []
+    , view = view
     }
 
 
@@ -68,25 +67,136 @@ viewPreview =
         ]
 
 
+view : State -> List (Html Msg)
+view state =
+    let
+        entriesSettings =
+            Control.currentValue state.settings
+                |> List.map Tuple.second
+    in
+    [ ControlView.view
+        { update = SetControls
+        , settings = state.settings
+        , toExampleCode =
+            \entries ->
+                [ { sectionName = "View"
+                  , code =
+                        String.join ""
+                            [ "SideNav.view"
+                            , "\n\t{ isCurrentRoute = isCurrentRoute"
+                            , "\n\t, routeToString = routeToString"
+                            , "\n\t, onSkipNav = SkipToContent"
+                            , "\n\t, css = []"
+                            , "\n\t}"
+                            , "\n\t[ "
+                            , String.join "\n\t" (List.map Tuple.first entries)
+                            , "\n\t]"
+                            ]
+                  }
+                ]
+        }
+    , SideNav.view
+        { isCurrentRoute =
+            -- TODO: show current route
+            \_ -> False
+        , routeToString = \_ -> ""
+        , onSkipNav = SkipToContent
+        , css = []
+        }
+        entriesSettings
+    ]
+
+
 {-| -}
 type alias State =
-    {}
+    { settings : Settings
+    }
+
+
+type alias Settings =
+    Control (List ( String, SideNav.Entry () Msg ))
 
 
 {-| -}
 init : State
 init =
-    {}
+    { settings =
+        ControlExtra.list
+            |> ControlExtra.listItem "entry" controlEntry
+    }
+
+
+controlEntry : Control ( String, SideNav.Entry () Msg )
+controlEntry =
+    Control.choice
+        [ ( "entry"
+          , Control.record
+                (\title attributes ->
+                    ( "SideNav.entry "
+                        ++ title
+                        ++ " [\n\t"
+                        ++ String.join "\n\t," (List.map Tuple.first attributes)
+                        ++ "\n\t]"
+                    , SideNav.entry title (List.map Tuple.second attributes)
+                    )
+                )
+                |> Control.field "title" (Control.string "Entry Category")
+                |> Control.field "attributes" controlEntryAttributes
+          )
+        , ( "entryWithChildren"
+          , Control.record
+                (\title attributes children ->
+                    ( "SideNav.entryWithChildren "
+                        ++ title
+                        ++ " [\n\t"
+                        ++ String.join "\n\t," (List.map Tuple.first attributes)
+                        ++ "\n\t]"
+                        ++ " [\n\t"
+                        ++ String.join "\n\t," (List.map Tuple.first children)
+                        ++ "\n\t]"
+                    , SideNav.entryWithChildren title
+                        (List.map Tuple.second attributes)
+                        (List.map Tuple.second children)
+                    )
+                )
+                |> Control.field "title" (Control.string "Entry Category")
+                |> Control.field "attributes" controlEntryAttributes
+                |> -- TODO: support sub-categories
+                   Control.field "children" (Control.value [])
+          )
+        , ( "html"
+          , Control.map
+                (\html ->
+                    ( "SideNav.html "
+                        ++ " [\n\t"
+                        ++ String.join "\n\t," (List.map Tuple.first html)
+                        ++ "\n\t]"
+                    , SideNav.html (List.map Tuple.second html)
+                    )
+                )
+                -- TODO: support HTML examples
+                (Control.value [])
+          )
+        ]
+
+
+controlEntryAttributes : Control (List ( String, SideNav.Attribute () Msg ))
+controlEntryAttributes =
+    ControlExtra.list
 
 
 {-| -}
-type alias Msg =
-    ()
+type Msg
+    = SetControls Settings
+    | SkipToContent
 
 
 {-| -}
 update : Msg -> State -> ( State, Cmd Msg )
 update msg state =
     case msg of
-        () ->
+        SetControls settings ->
+            ( { state | settings = settings }, Cmd.none )
+
+        SkipToContent ->
             ( state, Cmd.none )
