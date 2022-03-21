feb2b540bf81188417864b4a8f20f67a4a979e7a
Ben Dansby <dansby@noredink.com>
2022-03-21 08:31:34 -0700

    restyle

--- styleguide-app/Examples/IconExamples.elm
+++ styleguide-app/Examples/IconExamples.elm
@@ -1,6 +1,7 @@
 module Examples.IconExamples exposing (preview, view, viewWithCustomStyles)
 
 import Css
+import Css.Global
 import Html.Styled as Html exposing (Html)
 import Html.Styled.Attributes exposing (css)
 import Nri.Ui.Colors.V1 as Colors
@@ -42,23 +43,31 @@ view headerText icons =
 
 viewWithCustomStyles : String -> List ( String, Svg.Svg, List Css.Style ) -> Html msg
 viewWithCustomStyles headerText icons =
-    Html.section [ css [ Css.displayFlex, Css.alignItems Css.center ] ]
-        [ Html.div
-            [ css
-                [ Css.displayFlex
-                , Css.flexWrap Css.wrap
-                ]
+    Html.section
+        [ css
+            [ Css.displayFlex
+            , Css.alignItems Css.center
+            , Css.property "gap" "10px"
+            , Css.marginTop (Css.px 10)
             ]
-            (List.map viewIcon icons)
-        , Heading.h2
+        ]
+        [ Heading.h2
             [ Heading.css
-                [ Css.marginLeft (Css.px 32)
-                , Css.paddingLeft (Css.px 32)
-                , Css.borderLeft3 (Css.px 2) Css.solid Colors.gray92
+                [ Css.width (Css.px 150)
                 , Css.fontSize (Css.px 16)
+                , Css.lineHeight (Css.num 1.2)
+                , Css.fontWeight (Css.int 700)
                 ]
             ]
             [ Html.text headerText ]
+        , Html.div
+            [ css
+                [ Css.displayFlex
+                , Css.flexWrap Css.wrap
+                , Css.property "gap" "10px"
+                ]
+            ]
+            (List.map viewIcon icons)
         ]
 
 
@@ -68,11 +77,25 @@ viewIcon ( name, icon, style ) =
         [ css
             [ Css.displayFlex
             , Css.alignItems Css.center
-            , Css.margin (Css.px 8)
+            , Css.backgroundColor Colors.gray96
+            , Css.borderRadius (Css.px 8)
+            , Css.padding2 (Css.px 5) (Css.px 10)
+            , Css.hover
+                [ Css.backgroundColor Colors.glacier
+                , Css.color Colors.azure
+                , Css.Global.descendants
+                    [ Css.Global.selector "svg"
+                        [ Css.color Colors.azure
+                        ]
+                    ]
+                ]
             ]
         ]
         [ icon
             |> Svg.withCss style
             |> Svg.toHtml
-        , Text.smallBody [ Text.plaintext name ]
+        , Text.smallBody
+            [ Text.plaintext name
+            , Text.css [ Css.display Css.none ]
+            ]
         ]
