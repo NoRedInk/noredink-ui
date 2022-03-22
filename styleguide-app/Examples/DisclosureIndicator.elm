0c41188752a4143c077e919b79b2071d1be3d836
Tessa Kelly <tessakkelly@gmail.com>
2022-03-22 09:11:43 -0700

    Adds generated code to disclosure

--- styleguide-app/Examples/DisclosureIndicator.elm
+++ styleguide-app/Examples/DisclosureIndicator.elm
@@ -7,20 +7,15 @@ module Examples.DisclosureIndicator exposing (Msg, State, example)
 -}
 
 import Category exposing (Category(..))
-import Css
+import Css exposing (Style)
+import Debug.Control as Control exposing (Control)
+import Debug.Control.View as ControlView
 import Example exposing (Example)
 import Html.Styled as Html
 import Html.Styled.Attributes exposing (css)
 import Nri.Ui.Button.V10 as Button
 import Nri.Ui.DisclosureIndicator.V2 as DisclosureIndicator
 import Nri.Ui.Text.V6 as Text
-
-
-{-| -}
-type alias State =
-    { largeState : Bool
-    , mediumState : Bool
-    }
 
 
 {-| -}
@@ -42,6 +37,28 @@ example =
     , view =
         \state ->
             [ Text.smallBodyGray [ Text.plaintext "The disclosure indicator is only the caret. It is NOT a button -- you must create a button or clickabletext yourself!" ]
+            , ControlView.view
+                { update = UpdateSettings
+                , settings = state.settings
+                , toExampleCode =
+                    \settings ->
+                        let
+                            toCode viewName =
+                                "DisclosureIndicator."
+                                    ++ viewName
+                                    ++ " "
+                                    ++ Tuple.first settings.css
+                                    ++ " "
+                                    ++ Tuple.first settings.isOpen
+                        in
+                        [ { sectionName = "Large"
+                          , code = toCode "large"
+                          }
+                        , { sectionName = "medium"
+                          , code = toCode "medium"
+                          }
+                        ]
+                }
             , Html.div [ css [ Css.displayFlex, Css.padding (Css.px 8) ] ]
                 [ Button.button "Toggle large indicator"
                     [ Button.onClick ToggleLarge, Button.small, Button.secondary ]
@@ -61,16 +78,39 @@ example =
 
 
 {-| -}
-init : State
-init =
-    { largeState = False
-    , mediumState = False
+type alias State =
+    { settings : Control Settings
+    , largeState : Bool
+    , mediumState : Bool
     }
 
 
 {-| -}
+init : State
+init =
+    { settings = initSettings
+    , largeState = False
+    , mediumState = False
+    }
+
+
+type alias Settings =
+    { css : ( String, List Style )
+    , isOpen : ( String, Bool )
+    }
+
+
+initSettings : Control Settings
+initSettings =
+    Control.record Settings
+        |> Control.field "css" (Control.value ( "[]", [] ))
+        |> Control.field "isOpen" (Control.value ( "False", False ))
+
+
+{-| -}
 type Msg
-    = ToggleLarge
+    = UpdateSettings (Control Settings)
+    | ToggleLarge
     | ToggleMedium
 
 
@@ -78,6 +118,9 @@ type Msg
 update : Msg -> State -> ( State, Cmd Msg )
 update msg state =
     case msg of
+        UpdateSettings settings ->
+            ( { state | settings = settings }, Cmd.none )
+
         ToggleLarge ->
             ( { state | largeState = not state.largeState }, Cmd.none )
 
