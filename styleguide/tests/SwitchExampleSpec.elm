c0772233bb09c12627426fff2315388c1e029f5c
Tessa Kelly <tessakkelly@gmail.com>
2022-04-12 12:56:37 -0700

    Build out Elm test against styleguide app

--- styleguide/tests/SwitchExampleSpec.elm
+++ styleguide/tests/SwitchExampleSpec.elm
@@ -1,5 +1,6 @@
 module SwitchExampleSpec exposing (suite)
 
+import Accessibility.Aria as Aria
 import ProgramTest exposing (..)
 import Routes exposing (Route)
 import Test exposing (..)
@@ -19,5 +20,17 @@ suite =
             \() ->
                 app route
                     |> ensureViewHas [ text "Nri.Ui.Switch" ]
+                    -- switch starts with aria-checked=true and text "On"
+                    |> ensureViewHas
+                        [ attribute (Aria.checked (Just True))
+                        , text "On"
+                        ]
+                    -- user can click the first switch
+                    |> check "switch-interactive" "On" False
+                    -- the switch now has aria-checked=false and text "Off"
+                    |> ensureViewHas
+                        [ attribute (Aria.checked (Just False))
+                        , text "Off"
+                        ]
                     |> done
         ]
