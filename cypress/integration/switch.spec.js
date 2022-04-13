aad13687101c8ae4d0bde04629b4256f62acd8ce
Derek Blankenship <derek@noredink.com>
2022-04-13 15:13:24 -0700

    implement cypress proof of concept

--- cypress/integration/switch.spec.js
+++ cypress/integration/switch.spec.js
@@ -1,6 +1,43 @@
-/// <reference types="cypress" />
 describe('switch v1', () => {
   it('works', () => {
-    cy.log('hello world')
+    cy.visit('/#/doodad/Switch')
+    cy.percySnapshot()
+    cy.injectAxe()
+    assertInteractive()
+    assertAlwaysOn()
+    assertAlwaysOff()
   })
+
+  function assertInteractive () {
+    cy.checkA11y('[aria-controls=switch-interactive]')
+    cy.get('[aria-controls=switch-interactive]')
+      .should('have.attr', 'aria-checked', 'true')
+      .should('contain.text', 'On')
+      .click()
+      .should('have.attr', 'aria-checked', 'false')
+      .should('contain.text', 'Off')
+      .click()
+      .should('have.attr', 'aria-checked', 'true')
+      .should('contain.text', 'On')
+  }
+
+  function assertAlwaysOn () {
+    cy.checkA11y('[aria-controls=switch-disabled-on]')
+    cy.get('[aria-controls=switch-disabled-on]')
+      .should('have.attr', 'aria-checked', 'true')
+      .should('contain.text', 'Permanently on')
+      .click()
+      .should('have.attr', 'aria-checked', 'true')
+      .should('contain.text', 'Permanently on')
+  }
+
+  function assertAlwaysOff () {
+    cy.checkA11y('[aria-controls=switch-disabled-off]')
+    cy.get('[aria-controls=switch-disabled-off]')
+      .should('have.attr', 'aria-checked', 'false')
+      .should('contain.text', 'Permanently off')
+      .click()
+      .should('have.attr', 'aria-checked', 'false')
+      .should('contain.text', 'Permanently off')
+  }
 })
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,6 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import '@percy/cypress'
+import 'cypress-axe'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
