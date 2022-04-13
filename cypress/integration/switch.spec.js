8968bcdd3bbcbc514ed40eb9f29e11f66f063773
Derek Blankenship <derek@noredink.com>
2022-04-13 15:19:06 -0700

    refactor

--- cypress/integration/switch.spec.js
+++ cypress/integration/switch.spec.js
@@ -7,37 +7,37 @@ describe('switch v1', () => {
     assertAlwaysOn()
     assertAlwaysOff()
   })
-
-  function assertInteractive () {
-    cy.checkA11y('[aria-controls=switch-interactive]')
-    cy.get('[aria-controls=switch-interactive]')
-      .should('have.attr', 'aria-checked', 'true')
-      .should('contain.text', 'On')
-      .click()
-      .should('have.attr', 'aria-checked', 'false')
-      .should('contain.text', 'Off')
-      .click()
-      .should('have.attr', 'aria-checked', 'true')
-      .should('contain.text', 'On')
-  }
-
-  function assertAlwaysOn () {
-    cy.checkA11y('[aria-controls=switch-disabled-on]')
-    cy.get('[aria-controls=switch-disabled-on]')
-      .should('have.attr', 'aria-checked', 'true')
-      .should('contain.text', 'Permanently on')
-      .click()
-      .should('have.attr', 'aria-checked', 'true')
-      .should('contain.text', 'Permanently on')
-  }
-
-  function assertAlwaysOff () {
-    cy.checkA11y('[aria-controls=switch-disabled-off]')
-    cy.get('[aria-controls=switch-disabled-off]')
-      .should('have.attr', 'aria-checked', 'false')
-      .should('contain.text', 'Permanently off')
-      .click()
-      .should('have.attr', 'aria-checked', 'false')
-      .should('contain.text', 'Permanently off')
-  }
 })
+
+function assertInteractive () {
+  cy.checkA11y('[aria-controls=switch-interactive]')
+  cy.get('[aria-controls=switch-interactive]')
+    .should('have.attr', 'aria-checked', 'true')
+    .should('contain.text', 'On')
+    .click()
+    .should('have.attr', 'aria-checked', 'false')
+    .should('contain.text', 'Off')
+    .click()
+    .should('have.attr', 'aria-checked', 'true')
+    .should('contain.text', 'On')
+}
+
+function assertAlwaysOn () {
+  cy.checkA11y('[aria-controls=switch-disabled-on]')
+  cy.get('[aria-controls=switch-disabled-on]')
+    .should('have.attr', 'aria-checked', 'true')
+    .should('contain.text', 'Permanently on')
+    .click()
+    .should('have.attr', 'aria-checked', 'true')
+    .should('contain.text', 'Permanently on')
+}
+
+function assertAlwaysOff () {
+  cy.checkA11y('[aria-controls=switch-disabled-off]')
+  cy.get('[aria-controls=switch-disabled-off]')
+    .should('have.attr', 'aria-checked', 'false')
+    .should('contain.text', 'Permanently off')
+    .click()
+    .should('have.attr', 'aria-checked', 'false')
+    .should('contain.text', 'Permanently off')
+}
