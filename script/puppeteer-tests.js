47e57c559496a248b220a4be3eaf718f02e9c1b1
Tessa Kelly <tessakkelly@gmail.com>
2022-04-14 14:01:28 -0700

    Fix Balloon color contrast failure by switching to greenDarkest bg color

--- script/puppeteer-tests.js
+++ script/puppeteer-tests.js
@@ -71,7 +71,6 @@ describe('UI tests', function () {
 
   const skippedRules = {
     'Accordion': ['heading-order', 'region'],
-    'Balloon': ['color-contrast'],
     'Checkbox': ['region'],
     'Loading': ['color-contrast'],
     'Menu': ['region'],
