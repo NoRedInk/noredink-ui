167623b24dac8ca36ee13af43908dcf801419157
Tessa Kelly <tessakkelly@gmail.com>
2022-04-19 09:40:48 -0700

    Fix missing snapshots"

--- script/puppeteer-tests-percy.sh
+++ script/puppeteer-tests-percy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -euo pipefail
-npx percy exec -- mocha script/puppeteer-tests.js --timeout 50000 --exit
+env ONLYDOODAD=${1-default} npx percy exec -- mocha script/puppeteer-tests.js --timeout 50000 --exit
