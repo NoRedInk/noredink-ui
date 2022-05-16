507915d8ddbab23a752d7ffeeb2bfb64dc4efaeb
Tessa Kelly <tessakkelly@gmail.com>
2022-05-16 10:10:30 -0700

    Refactor to focus the tests on the pieces of info that matter

--- tests/Spec/ClickableAttributes.elm
+++ tests/Spec/ClickableAttributes.elm
@@ -1,6 +1,6 @@
 module Spec.ClickableAttributes exposing (suite)
 
-import ClickableAttributes
+import ClickableAttributes exposing (ClickableAttributes)
 import Expect
 import Html.Attributes exposing (href)
 import Html.Styled exposing (a, text, toUnstyled)
@@ -17,73 +17,62 @@ suite =
 
 validLinkAttributes : Test
 validLinkAttributes =
+    describe "link attributes"
+        [ test "with an href" <|
+            \() ->
+                ClickableAttributes.href "some-route"
+                    |> setupLinkTest
+                    |> Expect.all
+                        [ Query.has [ Selector.attribute (href "some-route") ]
+                        ]
+        , test "with an external href" <|
+            \() ->
+                ClickableAttributes.linkExternal "some-route"
+                    |> setupLinkTest
+                    |> Expect.all
+                        [ Query.has [ Selector.attribute (href "some-route") ]
+                        ]
+        , test "with an external href that also supports tracking" <|
+            \() ->
+                ClickableAttributes.linkExternalWithTracking { track = "track it!", url = "some-route" }
+                    |> setupLinkTest
+                    |> Expect.all
+                        [ Query.has [ Selector.attribute (href "some-route") ]
+                        ]
+        , test "with a SPA link" <|
+            \() ->
+                ClickableAttributes.linkSpa "some-route"
+                    |> setupLinkTest
+                    |> Expect.all
+                        [ Query.has [ Selector.attribute (href "some-route") ]
+                        ]
+        , test "with a link with a method" <|
+            \() ->
+                ClickableAttributes.linkWithMethod { method = "the right way", url = "some-route" }
+                    |> setupLinkTest
+                    |> Expect.all
+                        [ Query.has [ Selector.attribute (href "some-route") ]
+                        ]
+        , test "with a link with tracking" <|
+            \() ->
+                ClickableAttributes.linkWithTracking { track = "track it!", url = "some-route" }
+                    |> setupLinkTest
+                    |> Expect.all
+                        [ Query.has [ Selector.attribute (href "some-route") ]
+                        ]
+        ]
+
+
+setupLinkTest : (ClickableAttributes String msg -> ClickableAttributes String msg) -> Query.Single msg
+setupLinkTest withLink =
     let
         renderTestAnchorTag ( _, attributes ) =
             a attributes [ text "Test link" ]
                 |> toUnstyled
                 |> Query.fromHtml
     in
-    describe "link attributes"
-        [ test "with an href" <|
-            \() ->
-                ClickableAttributes.init
-                    |> ClickableAttributes.href "some-route"
-                    |> ClickableAttributes.toLinkAttributes
-                        { routeToString = identity, isDisabled = False }
-                    |> renderTestAnchorTag
-                    |> Expect.all
-                        [ Query.has [ Selector.attribute (href "some-route") ]
-                        ]
-        , test "with an external href" <|
-            \() ->
-                ClickableAttributes.init
-                    |> ClickableAttributes.linkExternal "some-route"
-                    |> ClickableAttributes.toLinkAttributes
-                        { routeToString = identity, isDisabled = False }
-                    |> renderTestAnchorTag
-                    |> Expect.all
-                        [ Query.has [ Selector.attribute (href "some-route") ]
-                        ]
-        , test "with an external href that also supports tracking" <|
-            \() ->
-                ClickableAttributes.init
-                    |> ClickableAttributes.linkExternalWithTracking
-                        { track = "track it!", url = "some-route" }
-                    |> ClickableAttributes.toLinkAttributes
-                        { routeToString = identity, isDisabled = False }
-                    |> renderTestAnchorTag
-                    |> Expect.all
-                        [ Query.has [ Selector.attribute (href "some-route") ]
-                        ]
-        , test "with a SPA link" <|
-            \() ->
-                ClickableAttributes.init
-                    |> ClickableAttributes.linkSpa "some-route"
-                    |> ClickableAttributes.toLinkAttributes
-                        { routeToString = identity, isDisabled = False }
-                    |> renderTestAnchorTag
-                    |> Expect.all
-                        [ Query.has [ Selector.attribute (href "some-route") ]
-                        ]
-        , test "with a link with a method" <|
-            \() ->
-                ClickableAttributes.init
-                    |> ClickableAttributes.linkWithMethod { method = "the right way", url = "some-route" }
-                    |> ClickableAttributes.toLinkAttributes
-                        { routeToString = identity, isDisabled = False }
-                    |> renderTestAnchorTag
-                    |> Expect.all
-                        [ Query.has [ Selector.attribute (href "some-route") ]
-                        ]
-        , test "with a link with tracking" <|
-            \() ->
-                ClickableAttributes.init
-                    |> ClickableAttributes.linkWithTracking
-                        { track = "track it!", url = "some-route" }
-                    |> ClickableAttributes.toLinkAttributes
-                        { routeToString = identity, isDisabled = False }
-                    |> renderTestAnchorTag
-                    |> Expect.all
-                        [ Query.has [ Selector.attribute (href "some-route") ]
-                        ]
-        ]
+    ClickableAttributes.init
+        |> withLink
+        |> ClickableAttributes.toLinkAttributes
+            { routeToString = identity, isDisabled = False }
+        |> renderTestAnchorTag
