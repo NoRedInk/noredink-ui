1b65fd629d9ced4fb9a6a8f9c8642036f7e3a5b3
Tessa Kelly <tessakkelly@gmail.com>
2022-05-24 17:31:06 -0700

    elm review

--- styleguide-app/Examples/Table.elm
+++ styleguide-app/Examples/Table.elm
@@ -13,7 +13,6 @@ import Debug.Control as Control exposing (Control)
 import Debug.Control.View as ControlView
 import Example exposing (Example)
 import Nri.Ui.Button.V10 as Button
-import Nri.Ui.Colors.V1 as Colors
 import Nri.Ui.Heading.V2 as Heading
 import Nri.Ui.Table.V5 as Table exposing (Column)
 
