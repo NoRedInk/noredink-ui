68bcda21abd2372caa4ba7691c41e0e42ce82331
Celso Bonutti <i.am@cel.so>
2022-06-16 12:07:11 -0300

    adds documentation to Carousel

--- src/Nri/Ui/Carousel/V1.elm
+++ src/Nri/Ui/Carousel/V1.elm
@@ -1,15 +1,34 @@
-module Nri.Ui.Carousel.V1 exposing (Item, buildItem, view)
+module Nri.Ui.Carousel.V1 exposing
+    ( view
+    , Item
+    , buildItem
+    )
+
+{-|
+
+@docs view
+@docs Item
+@docs buildItem
+
+-}
 
 import Css exposing (..)
 import Html.Styled as Html exposing (Html)
 import TabsInternal.V2 as TabsInternal
 
 
+{-| -}
 type Item id msg
     = Item (TabsInternal.Tab id msg)
 
 
-{-| -}
+{-| Builds an selectable item in the Caroursel
+
+`controlHtml` represents the element that will appear in the list of options.
+
+`slideHtml` represents the element that will be shown in your carousel when this item is selected.
+
+-}
 buildItem : { id : id, idString : String, slideHtml : Html msg, controlHtml : Html Never } -> Item id msg
 buildItem config =
     Item
@@ -20,12 +39,13 @@ buildItem config =
         )
 
 
+{-| -}
 view :
     { focusAndSelect : { select : id, focus : Maybe String } -> msg
     , selected : id
-    , items : List (Item id msg)
     , controlStyles : Bool -> List Style
     , controlListStyles : List Style
+    , items : List (Item id msg)
     }
     -> { controls : Html msg, slides : Html msg }
 view config =
