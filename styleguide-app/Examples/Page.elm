04257fb35198ba508246e97da8dc443b3c951579
Tessa Kelly <tessakkelly@gmail.com>
2022-07-14 13:23:08 -0700

    Use ControlView for Page settings

--- styleguide-app/Examples/Page.elm
+++ styleguide-app/Examples/Page.elm
@@ -7,9 +7,11 @@ module Examples.Page exposing (example, State, Msg)
 -}
 
 import Category exposing (Category(..))
+import Code
 import CommonControls
 import Css
 import Debug.Control as Control exposing (Control)
+import Debug.Control.View as ControlView
 import Example exposing (Example)
 import Html.Styled as Html exposing (Html)
 import Html.Styled.Attributes exposing (css)
@@ -17,21 +19,18 @@ import Http
 import Nri.Ui.Colors.V1 as Colors
 import Nri.Ui.Fonts.V1 as Fonts
 import Nri.Ui.Heading.V2 as Heading
-import Nri.Ui.Page.V3 as Page exposing (RecoveryText(..))
+import Nri.Ui.Page.V3 as Page exposing (DefaultPage, RecoveryText(..))
 
 
 {-| -}
 type alias State =
-    { httpError : Control Http.Error
-    , recoveryText : Control RecoveryText
-    }
+    Control Settings
 
 
 {-| -}
 type Msg
     = ShowItWorked String
-    | SetHttpError (Control Http.Error)
-    | SetRecoveryText (Control RecoveryText)
+    | UpdateSettings (Control Settings)
 
 
 update : Msg -> State -> ( State, Cmd Msg )
@@ -40,28 +39,28 @@ update msg model =
         ShowItWorked message ->
             ( Debug.log "Clicked: " message |> always model, Cmd.none )
 
-        SetHttpError controls ->
-            ( { model | httpError = controls }, Cmd.none )
+        UpdateSettings settings ->
+            ( settings, Cmd.none )
 
-        SetRecoveryText controls ->
-            ( { model | recoveryText = controls }, Cmd.none )
+
+moduleName : String
+moduleName =
+    "Page"
+
+
+version : Int
+version =
+    3
 
 
 {-| -}
 example : Example State Msg
 example =
-    { name = "Page"
-    , version = 3
+    { name = moduleName
+    , version = version
     , categories = [ Messaging ]
     , keyboardSupport = []
-    , state =
-        { httpError =
-            Control.record identity
-                |> Control.field "httpError" CommonControls.httpError
-        , recoveryText =
-            Control.record identity
-                |> Control.field "recoveryText" initRecoveryText
-        }
+    , state = controlSettings
     , update = update
     , subscriptions = \_ -> Sub.none
     , preview =
@@ -93,29 +92,29 @@ example =
     , view =
         \ellieLinkConfig model ->
             let
-                recoveryText =
-                    Control.currentValue model.recoveryText
+                settings =
+                    Control.currentValue model
             in
-            [ Html.fromUnstyled (Control.view SetRecoveryText model.recoveryText)
-            , viewExample "Page.httpError error" (Page.httpError (Control.currentValue model.httpError)) recoveryText [ Html.fromUnstyled (Control.view SetHttpError model.httpError) ]
-            , viewExample "Page.broken" Page.broken recoveryText []
-            , viewExample "Page.blockedV4" (Page.blockedV4 "Error message details") recoveryText []
-            , viewExample "Page.notFound" Page.notFound recoveryText []
-            , viewExample "Page.noPermission" Page.noPermission recoveryText []
-            , viewExample "Page.loggedOut" Page.loggedOut recoveryText []
-            , viewExample "Page.timeOut" Page.timeOut recoveryText []
-            , viewExample "Page.networkError" Page.networkError recoveryText []
+            [ ControlView.view
+                { ellieLinkConfig = ellieLinkConfig
+                , name = moduleName
+                , version = version
+                , update = UpdateSettings
+                , settings = model
+                , mainType = "RootHtml.Html msg"
+                , extraImports = [ "import Http" ]
+                , toExampleCode = \_ -> []
+                }
+            , viewExample settings.page settings.recoveryText
             ]
     }
 
 
 viewExample :
-    String
-    -> (Page.DefaultPage Msg -> Html Msg)
+    ( String, Page.DefaultPage Msg -> Html Msg )
     -> RecoveryText
-    -> List (Html Msg)
     -> Html Msg
-viewExample viewName view recoveryText extras =
+viewExample ( viewName, view ) recoveryText =
     Html.div
         [ css
             [ Css.marginTop (Css.px 20)
@@ -125,7 +124,6 @@ viewExample viewName view recoveryText extras =
             ]
         ]
         [ Heading.h2 [ Heading.style Heading.Subhead ] [ Html.text viewName ]
-        , Html.div [] extras
         , Html.code []
             [ Html.text <|
                 viewName
@@ -135,6 +133,50 @@ viewExample viewName view recoveryText extras =
             ]
         , view { link = ShowItWorked viewName, recoveryText = recoveryText }
         ]
+
+
+type alias Settings =
+    { page : ( String, DefaultPage Msg -> Html Msg )
+    , recoveryText : RecoveryText
+    }
+
+
+controlSettings : Control Settings
+controlSettings =
+    Control.record Settings
+        |> Control.field "page" controlPageType
+        |> Control.field "recoveryText" initRecoveryText
+
+
+controlPageType : Control ( String, DefaultPage Msg -> Html Msg )
+controlPageType =
+    let
+        choiceWithModuleName name value =
+            ( name, Control.value ( moduleName ++ "." ++ name, value ) )
+    in
+    [ ( "httpError"
+      , Control.map
+            (\err ->
+                ( moduleName ++ ".httpError httpError -- Use Http.error"
+                , Page.httpError err
+                )
+            )
+            CommonControls.httpError
+      )
+    , choiceWithModuleName "broken" Page.broken
+    , ( "blockedV4"
+      , Control.value
+            ( "Page.blockedV4 " ++ Code.string "Error message details"
+            , Page.blockedV4 "Error message details"
+            )
+      )
+    , choiceWithModuleName "notFound" Page.notFound
+    , choiceWithModuleName "noPermission" Page.noPermission
+    , choiceWithModuleName "loggedOut" Page.loggedOut
+    , choiceWithModuleName "timeOut" Page.timeOut
+    , choiceWithModuleName "networkError" Page.networkError
+    ]
+        |> Control.choice
 
 
 initRecoveryText : Control RecoveryText
