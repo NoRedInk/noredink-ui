0019760a8ec0bd7cb11549c584574fb8335c2b3d
Tessa Kelly <tessakkelly@gmail.com>
2022-10-06 12:21:36 -0600

    cp Nri.Panel from monolith

---
+++ src/Nri/Ui/Panel/V1.elm
@@ -0,0 +1,180 @@
+module Nri.Panel exposing
+    ( view
+    , Customization, primaryTheme, secondaryTheme, accordionLikeSpacing
+    )
+
+{-| Create panels (AKA wells.)
+
+
+## View
+
+`view` works mostly like the functions from `Html`, but you can only customize
+it with things that make sense for panels. To get a really basic panel:
+
+    view []
+        "Header Text"
+        [ text "Body Content" ]
+
+@docs view
+
+
+## Customizations
+
+Customizations work more-or-less like very specific `Html.Attribute`s. Use them
+like this:
+
+    view
+        [ secondaryTheme ]
+        "Header text"
+        [ text "Body Content" ]
+
+@docs Customization, primaryTheme, secondaryTheme, accordionLikeSpacing
+
+You can also add extra stuff to the right side of the panel header. This is a
+customization, despite being content. See the examples in the style guide for
+how this works out.
+
+-}
+
+import Css
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (css)
+import Nri.Ui.Colors.V1 as Colors
+import Nri.Ui.Fonts.V1 as Fonts
+
+
+type alias Panel msg =
+    { header : String
+    , headerExtras : List (Html msg)
+    , contents : List (Html msg)
+    , theme : Theme
+    , spacing : Spacing
+    }
+
+
+init : String -> List (Html msg) -> Panel msg
+init header contents =
+    { header = header
+    , headerExtras = []
+    , contents = contents
+    , theme = Primary
+    , spacing = StandardHeight
+    }
+
+
+{-| A single customization for a panel. You can think of this like a very
+specific `Html.Attribute`.
+-}
+type Customization msg
+    = WithTheme Theme
+    | WithSpacing Spacing
+
+
+type Theme
+    = Primary
+    | Secondary
+
+
+{-| use the primary color theme (this is the default)
+-}
+primaryTheme : Customization msg
+primaryTheme =
+    WithTheme Primary
+
+
+{-| use the secondary color theme
+-}
+secondaryTheme : Customization msg
+secondaryTheme =
+    WithTheme Secondary
+
+
+{-| how much should this panel have above and below it? Should we use a
+reasonable margin or collapse it like an accordion?
+-}
+type Spacing
+    = StandardHeight
+    | AccordionLike
+
+
+{-| space panels like accordion components. This also limits their width (see
+the style guide.)
+-}
+accordionLikeSpacing : Customization msg
+accordionLikeSpacing =
+    WithSpacing AccordionLike
+
+
+customize : Customization msg -> Panel msg -> Panel msg
+customize customization panel =
+    case customization of
+        WithTheme theme ->
+            { panel | theme = theme }
+
+        WithSpacing spacing ->
+            { panel | spacing = spacing }
+
+
+
+-- views
+
+
+{-| create a panel, given the options you specify
+-}
+view : List (Customization msg) -> String -> List (Html msg) -> Html msg
+view customizations header contents =
+    let
+        panel =
+            List.foldl customize (init header contents) customizations
+    in
+    section []
+        [ Html.Styled.header
+            [ css
+                [ case panel.theme of
+                    Primary ->
+                        Css.backgroundColor Colors.navy
+
+                    Secondary ->
+                        Css.backgroundColor Colors.gray75
+                , case panel.spacing of
+                    StandardHeight ->
+                        Css.batch
+                            [ Css.padding2 (Css.px 8) (Css.px 15)
+                            ]
+
+                    AccordionLike ->
+                        Css.batch
+                            [ Css.padding2 (Css.px 0) (Css.px 16)
+                            , Css.lineHeight (Css.px 30)
+                            ]
+                , Fonts.baseFont
+                , Css.fontSize (Css.px 16)
+                , Css.color Colors.white
+                , Css.borderTopLeftRadius (Css.px 8)
+                , Css.borderTopRightRadius (Css.px 8)
+                , Css.displayFlex
+                , Css.alignItems Css.center
+                , Css.flexWrap Css.wrap
+                ]
+            ]
+            (text panel.header :: panel.headerExtras)
+        , article
+            [ css
+                [ case panel.spacing of
+                    StandardHeight ->
+                        Css.padding2 (Css.px 8) (Css.px 15)
+
+                    AccordionLike ->
+                        Css.marginBottom (Css.px 10)
+                , Fonts.baseFont
+                , Css.fontSize (Css.px 16)
+                , Css.color Colors.gray20
+                , Css.backgroundColor Colors.white
+                , Css.borderBottomLeftRadius (Css.px 8)
+                , Css.borderBottomRightRadius (Css.px 8)
+                , Css.overflowWrap Css.breakWord
+                , Css.property "word-wrap" "break-word"
+                ]
+            ]
+            panel.contents
+        ]
