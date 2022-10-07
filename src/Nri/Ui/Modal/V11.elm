fc536d0e8712debb6d9f567f5a0b314978f729b5
Ben Dansby <dansby@noredink.com>
2022-10-07 15:26:35 -0700

    Update V11.elm

--- src/Nri/Ui/Modal/V11.elm
+++ src/Nri/Ui/Modal/V11.elm
@@ -662,7 +662,7 @@ viewInnerContent ({ visibleTitle } as config) =
                     , Css.maxHeight
                         (Css.calc (Css.vh 100)
                             Css.minus
-                            (Css.px (footerMobileHeight + titleMobileHeight + 40))
+                            (Css.px (footerMobileHeight + titleMobileHeight + 80))
                         )
                     ]
                 , if visibleTitle then
