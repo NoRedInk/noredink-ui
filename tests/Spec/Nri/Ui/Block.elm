2d89306931b666f25bd90c2fdc22a5a2623ee3b0
Tessa Kelly <tessakkelly@gmail.com>
2022-12-15 14:38:46 -0700

    Adds getLableHeights tests"

--- tests/Spec/Nri/Ui/Block.elm
+++ tests/Spec/Nri/Ui/Block.elm
@@ -1,5 +1,7 @@
 module Spec.Nri.Ui.Block exposing (spec)
 
+import Browser.Dom exposing (Element)
+import Dict
 import Expect
 import Html.Styled
 import Nri.Ui.Block.V1 as Block
@@ -12,6 +14,7 @@ spec : Test
 spec =
     describe "Nri.Ui.Block.V1"
         [ describe "content" contentSpec
+        , describe "getLabelHeights" getLabelHeightsSpec
         ]
 
 
@@ -44,3 +47,61 @@ toQuery block =
         |> Html.Styled.p []
         |> Html.Styled.toUnstyled
         |> Query.fromHtml
+
+
+getLabelHeightsSpec : List Test
+getLabelHeightsSpec =
+    [ test "without any ids or measurements, does not specify heights" <|
+        \() ->
+            Block.getLabelHeights [] Dict.empty
+                |> Expect.equal Dict.empty
+    , test "without any measurements, does not specify heights" <|
+        \() ->
+            Block.getLabelHeights [ "a" ] Dict.empty
+                |> Expect.equal Dict.empty
+    , test "without any ids, does not specify heights" <|
+        \() ->
+            Block.getLabelHeights []
+                (Dict.singleton "a"
+                    { label = dummyElement { x = 0, y = 0, width = 100, height = 100 }
+                    , labelContent = dummyElement { x = 0, y = 0, width = 100, height = 20 }
+                    }
+                )
+                |> Expect.equal Dict.empty
+    , test "with 1 id and 1 measurement, specifies the default heights" <|
+        \() ->
+            let
+                startingHeight =
+                    20
+            in
+            Block.getLabelHeights [ "a" ]
+                (Dict.singleton "a"
+                    { label = dummyElement { x = 0, y = 0, width = 100, height = 100 }
+                    , labelContent = dummyElement { x = 0, y = 0, width = 100, height = 20 }
+                    }
+                )
+                |> Expect.equal
+                    (Dict.singleton "a"
+                        { totalHeight = startingHeight + defaultArrowHeight + balloonOffset
+                        , arrowHeight = defaultArrowHeight
+                        }
+                    )
+    ]
+
+
+balloonOffset : Float
+balloonOffset =
+    8
+
+
+defaultArrowHeight : Float
+defaultArrowHeight =
+    8
+
+
+dummyElement : { x : Float, y : Float, width : Float, height : Float } -> Element
+dummyElement element =
+    { scene = { width = 1000, height = 1000 }
+    , viewport = { x = 0, y = 0, width = 1000, height = 500 }
+    , element = element
+    }
