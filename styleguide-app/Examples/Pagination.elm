4393c12361799b5a944cfc1018f13224ceceecf1
Tessa Kelly <tessakkelly@gmail.com>
2022-12-29 16:06:55 -0700

    Adds Pagination to styleguide example

---
+++ styleguide-app/Examples/Pagination.elm
@@ -0,0 +1,109 @@
+module Examples.Pagination exposing (example, State, Msg)
+
+{-|
+
+@docs example, State, Msg
+
+-}
+
+import Category exposing (Category(..))
+import Code
+import CommonControls
+import Css
+import Debug.Control as Control exposing (Control)
+import Debug.Control.Extra as ControlExtra
+import Debug.Control.View as ControlView
+import Example exposing (Example)
+import Html.Styled as Html exposing (Html)
+import Html.Styled.Attributes exposing (css)
+import Nri.Ui.Colors.V1 as Colors
+import Nri.Ui.Fonts.V1 as Fonts
+import Nri.Ui.Heading.V3 as Heading
+import Nri.Ui.Pagination.V1 as Pagination
+
+
+moduleName : String
+moduleName =
+    "Pagination"
+
+
+version : Int
+version =
+    1
+
+
+{-| -}
+example : Example State Msg
+example =
+    { name = moduleName
+    , version = version
+    , categories = [ Layout ]
+    , keyboardSupport = []
+    , state = init
+    , update = update
+    , subscriptions = \_ -> Sub.none
+    , preview =
+        [-- TODO
+        ]
+    , view =
+        \ellieLinkConfig model ->
+            let
+                settings =
+                    Control.currentValue model.settings
+            in
+            [ ControlView.view
+                { ellieLinkConfig = ellieLinkConfig
+                , name = moduleName
+                , version = version
+                , update = UpdateControls
+                , settings = model.settings
+                , mainType = Nothing
+                , extraCode = []
+                , renderExample = Code.unstyledView
+                , toExampleCode = \_ -> []
+                }
+            , Heading.h2 [ Heading.plaintext "Example" ]
+            , Pagination.view SelectPage model.currentPage (List.range 1 settings.pages)
+            ]
+    }
+
+
+{-| -}
+type alias State =
+    { currentPage : Int
+    , settings : Control Settings
+    }
+
+
+init : State
+init =
+    { currentPage = 0
+    , settings = controlSettings
+    }
+
+
+type alias Settings =
+    { pages : Int
+    }
+
+
+controlSettings : Control Settings
+controlSettings =
+    Control.record Settings
+        |> Control.field "Page count" (ControlExtra.int 6)
+
+
+{-| -}
+type Msg
+    = UpdateControls (Control Settings)
+    | SelectPage Int
+
+
+update : Msg -> State -> ( State, Cmd Msg )
+update msg model =
+    case msg of
+        UpdateControls settings ->
+            ( { model | settings = settings }, Cmd.none )
+
+        SelectPage i ->
+            ( { model | currentPage = i }, Cmd.none )
