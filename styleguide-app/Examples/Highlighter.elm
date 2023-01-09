1315b2c2ef52e0c45ec8a834fa7cd0bc48453302
Tessa Kelly <tessakkelly@gmail.com>
2023-01-09 13:00:55 -0700

    Cp Highlighter.V1 to V2

--- styleguide-app/Examples/Highlighter.elm
+++ styleguide-app/Examples/Highlighter.elm
@@ -21,7 +21,7 @@ import Nri.Ui.Colors.V1 as Colors
 import Nri.Ui.Fonts.V1 as Fonts
 import Nri.Ui.Heading.V3 as Heading
 import Nri.Ui.Highlightable.V1 as Highlightable exposing (Highlightable)
-import Nri.Ui.Highlighter.V1 as Highlighter
+import Nri.Ui.Highlighter.V2 as Highlighter
 import Nri.Ui.HighlighterTool.V1 as Tool
 import Nri.Ui.Table.V6 as Table
 
@@ -33,7 +33,7 @@ moduleName =
 
 version : Int
 version =
-    1
+    2
 
 
 {-| -}
