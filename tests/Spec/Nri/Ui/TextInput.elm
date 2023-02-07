6a782a1f865a838ec5f5aaa366b4ab8592029b68
Lindsay Wardell <lindsay.wardell@noredink.com>
2023-02-07 10:07:54 -0800

    Add some basic tests

--- tests/Spec/Nri/Ui/TextInput.elm
+++ tests/Spec/Nri/Ui/TextInput.elm
@@ -1,6 +1,8 @@
 module Spec.Nri.Ui.TextInput exposing (spec)
 
+import Html.Attributes as Attr
 import Html.Styled
+import Iso8601
 import Nri.Ui.TextInput.V7 as TextInput
 import Test exposing (..)
 import Test.Html.Query as Query
@@ -20,4 +22,48 @@ spec =
                         [ tag "input"
                         , id (TextInput.generateId "myLabel")
                         ]
+        , describe "datetime"
+            [ test "it displays no value when no value is provided" <|
+                \() ->
+                    TextInput.view "myLabel"
+                        [ TextInput.datetime identity, TextInput.value Nothing ]
+                        |> Html.Styled.toUnstyled
+                        |> Query.fromHtml
+                        |> Query.has
+                            [ tag "input"
+                            , attribute (Attr.value "")
+                            ]
+            , test "it displays the value when a value is provided" <|
+                \() ->
+                    TextInput.view "myLabel"
+                        [ TextInput.datetime identity, TextInput.value (Iso8601.toTime "2018-01-01T00:00:00.000Z" |> Result.toMaybe) ]
+                        |> Html.Styled.toUnstyled
+                        |> Query.fromHtml
+                        |> Query.has
+                            [ tag "input"
+                            , attribute (Attr.value "2018-01-01T00:00:00.000")
+                            ]
+            ]
+        , describe "date"
+            [ test "it displays no value when no value is provided" <|
+                \() ->
+                    TextInput.view "myLabel"
+                        [ TextInput.date identity, TextInput.value Nothing ]
+                        |> Html.Styled.toUnstyled
+                        |> Query.fromHtml
+                        |> Query.has
+                            [ tag "input"
+                            , attribute (Attr.value "")
+                            ]
+            , test "it displays the value when a value is provided" <|
+                \() ->
+                    TextInput.view "myLabel"
+                        [ TextInput.date identity, TextInput.value (Iso8601.toTime "2018-01-01" |> Result.toMaybe) ]
+                        |> Html.Styled.toUnstyled
+                        |> Query.fromHtml
+                        |> Query.has
+                            [ tag "input"
+                            , attribute (Attr.value "2018-01-01")
+                            ]
+            ]
         ]
