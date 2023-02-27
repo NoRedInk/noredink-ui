7f2b74f9725527dcb68df6ff055b41d7f50d9e99
Tessa Kelly <tessakkelly@gmail.com>
2023-02-27 12:30:47 -0700

    :pencil: doc updates

--- src/Nri/Ui/RadioButton/V4.elm
+++ src/Nri/Ui/RadioButton/V4.elm
@@ -9,7 +9,16 @@ module Nri.Ui.RadioButton.V4 exposing
     , disabled, enabled, errorIf, errorMessage, guidance
     )
 
-{-| Changes from V3:
+{-|
+
+
+### Patch changes:
+
+  - replace `height` use with `minHeight` to prevent vertical text overflow issues
+  - use `break-word` to prevent horiztonal text overflow issues
+
+
+### Changes from V3:
 
   - use PremiumDisplay instead of PremiumLevel
   - rename showPennant to onLockedClick since its display depends on premium now
