98d2006cc4057094413d3ec6d657f717a0467c97
Micah Hahn <micah.s.hahn@gmail.com>
2023-05-30 17:40:31 -0500

    Add placeholders for new character SVGs

---
+++ src/Nri/Ui/CharacterIcon/V2.elm
@@ -0,0 +1,74 @@
+module Nri.Ui.CharacterIcon.V2 exposing (..)
+
+{-|
+
+@docs lindyHeadshot, lindyInstructive, lindySupportive
+@docs redHeadshot, redInstructive, redSupportive
+@docs salHeadshot, salInstructive, salSupportive
+
+-}
+
+import Nri.Ui.Svg.V1
+
+
+{-| -}
+lindyHeadshot : Nri.Ui.Svg.V1.Svg
+lindyHeadshot =
+    Nri.Ui.Svg.V1.init ""
+        []
+
+
+{-| -}
+lindyInstructive : Nri.Ui.Svg.V1.Svg
+lindyInstructive =
+    Nri.Ui.Svg.V1.init ""
+        []
+
+
+{-| -}
+lindySupportive : Nri.Ui.Svg.V1.Svg
+lindySupportive =
+    Nri.Ui.Svg.V1.init ""
+        []
+
+
+{-| -}
+redHeadshot : Nri.Ui.Svg.V1.Svg
+redHeadshot =
+    Nri.Ui.Svg.V1.init ""
+        []
+
+
+{-| -}
+redInstructive : Nri.Ui.Svg.V1.Svg
+redInstructive =
+    Nri.Ui.Svg.V1.init ""
+        []
+
+
+{-| -}
+redSupportive : Nri.Ui.Svg.V1.Svg
+redSupportive =
+    Nri.Ui.Svg.V1.init ""
+        []
+
+
+{-| -}
+salHeadshot : Nri.Ui.Svg.V1.Svg
+salHeadshot =
+    Nri.Ui.Svg.V1.init ""
+        []
+
+
+{-| -}
+salInstructive : Nri.Ui.Svg.V1.Svg
+salInstructive =
+    Nri.Ui.Svg.V1.init ""
+        []
+
+
+{-| -}
+salSupportive : Nri.Ui.Svg.V1.Svg
+salSupportive =
+    Nri.Ui.Svg.V1.init ""
+        []
