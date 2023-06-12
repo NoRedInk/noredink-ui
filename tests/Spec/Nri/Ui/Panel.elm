990f16e13d0a4327b44b50c38f2ccaaac034282b
charbelrami <charbel.f.rami@gmail.com>
2023-06-12 11:24:30 -0300

    add panel tests

---
+++ tests/Spec/Nri/Ui/Panel.elm
@@ -0,0 +1,94 @@
+module Spec.Nri.Ui.Panel exposing (..)
+
+import Html
+import Html.Styled exposing (..)
+import Nri.Ui.Panel.V1 as Panel
+import ProgramTest exposing (..)
+import Test exposing (..)
+import Test.Html.Query as Query
+
+
+spec : Test
+spec =
+    describe "Nri.Ui.Panel.V1"
+        [ describe "renders content" rendersContent
+        ]
+
+
+rendersContent : List Test
+rendersContent =
+    [ test "renders markdown correctly" <|
+        \() ->
+            programMarkdown
+                |> ensureView
+                    (Query.contains
+                        [ Html.em [] [ Html.text "italic" ], Html.strong [] [ Html.text "bold" ] ]
+                    )
+                |> done
+    , test "renders html correctly" <|
+        \() ->
+            programHtml
+                |> ensureView
+                    (Query.contains
+                        [ Html.em [] [ Html.text "italic" ], Html.strong [] [ Html.text "bold" ] ]
+                    )
+                |> done
+    ]
+
+
+type alias Model =
+    {}
+
+
+init : Model
+init =
+    {}
+
+
+type Msg
+    = Toggle Bool
+
+
+update : Msg -> Model -> Model
+update msg state =
+    state
+
+
+viewMarkdown : Model -> Html Msg
+viewMarkdown model =
+    Panel.view
+        [ Panel.header "Header"
+        , Panel.markdown "_italic_ **bold**"
+        ]
+
+
+viewHtml : Model -> Html Msg
+viewHtml model =
+    Panel.view
+        [ Panel.header "Header"
+        , Panel.html [ em [] [ text "italic" ], strong [] [ text "bold" ] ]
+        ]
+
+
+type alias TestContext =
+    ProgramTest Model Msg ()
+
+
+programMarkdown : TestContext
+programMarkdown =
+    ProgramTest.createSandbox
+        { init = init
+        , update = update
+        , view = viewMarkdown >> toUnstyled
+        }
+        |> ProgramTest.start ()
+
+
+programHtml : TestContext
+programHtml =
+    ProgramTest.createSandbox
+        { init = init
+        , update = update
+        , view = viewHtml >> toUnstyled
+        }
+        |> ProgramTest.start ()
