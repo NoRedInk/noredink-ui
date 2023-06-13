6f5cfcfa184838351897908f64df19df69e0e5ae
charbelrami <charbel.f.rami@gmail.com>
2023-06-13 14:01:36 -0300

    remove unused boilerplate form Panel spec

--- tests/Spec/Nri/Ui/Panel.elm
+++ tests/Spec/Nri/Ui/Panel.elm
@@ -3,7 +3,6 @@ module Spec.Nri.Ui.Panel exposing (..)
 import Html
 import Html.Styled exposing (..)
 import Nri.Ui.Panel.V1 as Panel
-import ProgramTest exposing (..)
 import Test exposing (..)
 import Test.Html.Query as Query
 
@@ -19,67 +18,24 @@ rendersContent : List Test
 rendersContent =
     [ test "renders markdown correctly" <|
         \() ->
-            programMarkdown
-                |> ensureView
-                    (Query.contains
-                        [ Html.em [] [ Html.text "italic" ], Html.strong [] [ Html.text "bold" ] ]
-                    )
-                |> done
+            Panel.view
+                [ Panel.header "Header"
+                , Panel.markdown "_italic_ **bold**"
+                ]
+                |> toUnstyled
+                |> Query.fromHtml
+                |> Query.contains
+                    [ Html.em [] [ Html.text "italic" ]
+                    , Html.strong [] [ Html.text "bold" ]
+                    ]
     , test "renders html correctly" <|
         \() ->
-            programHtml
-                |> ensureView
-                    (Query.contains
-                        [ Html.em [] [ Html.text "italic" ], Html.strong [] [ Html.text "bold" ] ]
-                    )
-                |> done
+            Panel.view
+                [ Panel.header "Header"
+                , Panel.html [ em [] [ text "italic" ], strong [] [ text "bold" ] ]
+                ]
+                |> toUnstyled
+                |> Query.fromHtml
+                |> Query.contains
+                    [ Html.em [] [ Html.text "italic" ], Html.strong [] [ Html.text "bold" ] ]
     ]
-
-
-type alias Model =
-    {}
-
-
-init : Model
-init =
-    {}
-
-
-viewMarkdown : Model -> Html msg
-viewMarkdown model =
-    Panel.view
-        [ Panel.header "Header"
-        , Panel.markdown "_italic_ **bold**"
-        ]
-
-
-viewHtml : Model -> Html msg
-viewHtml model =
-    Panel.view
-        [ Panel.header "Header"
-        , Panel.html [ em [] [ text "italic" ], strong [] [ text "bold" ] ]
-        ]
-
-
-type alias TestContext =
-    ProgramTest Model () ()
-
-
-programMarkdown : TestContext
-programMarkdown =
-    ProgramTest.createSandbox
-        { init = init
-        , update = \msg model -> model
-        , view = viewMarkdown >> toUnstyled
-        }
-        |> ProgramTest.start ()
-
-
-programHtml : TestContext
-programHtml =
-    ProgramTest.createSandbox
-        { init = init
-        , update = \msg model -> model
-        , view = viewHtml >> toUnstyled
-        }
-        |> ProgramTest.start ()
