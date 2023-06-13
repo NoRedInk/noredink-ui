7437ba7346256118622c30685fe7bacc1790b37b
charbelrami <charbel.f.rami@gmail.com>
2023-06-13 14:07:08 -0300

    remove unused boilerplate from Switch spec

--- tests/Spec/Nri/Ui/Switch.elm
+++ tests/Spec/Nri/Ui/Switch.elm
@@ -4,8 +4,8 @@ import Accessibility.Aria as Aria
 import Accessibility.Role as Role
 import Html.Styled exposing (..)
 import Nri.Ui.Switch.V2 as Switch
-import ProgramTest exposing (..)
 import Test exposing (..)
+import Test.Html.Query as Query
 import Test.Html.Selector as Selector
 
 
@@ -21,9 +21,12 @@ hasCorrectRole : List Test
 hasCorrectRole =
     [ test "has role 'switch'" <|
         \() ->
-            program
-                |> ensureViewHas [ Selector.attribute Role.switch ]
-                |> done
+            Switch.view { id = "switch", label = "Switch" }
+                []
+                |> toUnstyled
+                |> Query.fromHtml
+                |> Query.find [ Selector.id "switch" ]
+                |> Query.has [ Selector.attribute Role.switch ]
     ]
 
 
@@ -31,78 +34,10 @@ hasCorrectAriaDisabled : List Test
 hasCorrectAriaDisabled =
     [ test "has 'aria-disabled=true' when disabled" <|
         \() ->
-            programDisabled
-                |> ensureViewHas [ Selector.attribute (Aria.disabled True) ]
-                |> done
+            Switch.view { id = "switch", label = "Switch" }
+                [ Switch.disabled True ]
+                |> toUnstyled
+                |> Query.fromHtml
+                |> Query.find [ Selector.id "switch" ]
+                |> Query.has [ Selector.attribute (Aria.disabled True) ]
     ]
-
-
-type alias Model =
-    { selected : Bool
-    }
-
-
-init : Model
-init =
-    { selected = False
-    }
-
-
-type Msg
-    = Toggle Bool
-
-
-update : Msg -> Model -> Model
-update msg state =
-    case msg of
-        Toggle selected ->
-            let
-                selected_ =
-                    if selected then
-                        False
-
-                    else
-                        True
-            in
-            { state | selected = selected_ }
-
-
-view : Model -> Html Msg
-view model =
-    Switch.view { id = "switch", label = "Switch" }
-        [ Switch.selected model.selected
-        , Switch.onSwitch Toggle
-        ]
-
-
-viewDisabled : Model -> Html Msg
-viewDisabled model =
-    Switch.view { id = "switch", label = "Switch" }
-        [ Switch.selected model.selected
-        , Switch.onSwitch Toggle
-        , Switch.disabled True
-        ]
-
-
-type alias TestContext =
-    ProgramTest Model Msg ()
-
-
-program : TestContext
-program =
-    ProgramTest.createSandbox
-        { init = init
-        , update = update
-        , view = view >> toUnstyled
-        }
-        |> ProgramTest.start ()
-
-
-programDisabled : TestContext
-programDisabled =
-    ProgramTest.createSandbox
-        { init = init
-        , update = update
-        , view = viewDisabled >> toUnstyled
-        }
-        |> ProgramTest.start ()
