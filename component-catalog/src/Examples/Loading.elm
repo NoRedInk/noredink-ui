528eec7ad6534e49d9d56abf34696f02987dc2d3
Tessa Kelly <tessakkelly@gmail.com>
2023-08-07 17:31:56 -0600

    Adds missing heading to Loading examples

--- component-catalog/src/Examples/Loading.elm
+++ component-catalog/src/Examples/Loading.elm
@@ -16,6 +16,7 @@ import Html.Styled.Events as Events
 import Json.Decode
 import Nri.Ui.Button.V10 as Button
 import Nri.Ui.Colors.V1 as Colors
+import Nri.Ui.Heading.V3 as Heading
 import Nri.Ui.Loading.V1 as Loading
 import Nri.Ui.Svg.V1 as Svg
 
@@ -102,7 +103,11 @@ example =
     , about = Guidance.useATACGuide moduleName
     , view =
         \ellieLinkConfig { showLoadingFadeIn, showLoading, showSpinners } ->
-            [ if showLoading then
+            [ Heading.h2
+                [ Heading.plaintext "Examples"
+                , Heading.css [ Css.marginBottom (Css.px 30) ]
+                ]
+            , if showLoading then
                 Loading.page
 
               else
