cf0a8fe095038840eb3ba08dd94c94070094426a
Tessa Kelly <tessakkelly@gmail.com>
2023-09-01 17:11:13 -0600

    Adds very basic starting point for a form

--- component-catalog/src/UsageExamples.elm
+++ component-catalog/src/UsageExamples.elm
@@ -2,6 +2,7 @@ module UsageExamples exposing (Msg, State, all)
 
 import UsageExample exposing (UsageExample)
 import UsageExamples.ClickableCardWithTooltip as ClickableCardWithTooltip
+import UsageExamples.Form as Form
 
 
 all : List (UsageExample State Msg)
@@ -12,19 +13,46 @@ all =
                 case msg of
                     ClickableCardWithTooltipMsg childMsg ->
                         Just childMsg
+
+                    _ ->
+                        Nothing
             )
         |> UsageExample.wrapState ClickableCardWithTooltipState
             (\msg ->
                 case msg of
                     ClickableCardWithTooltipState childState ->
                         Just childState
+
+                    _ ->
+                        Nothing
+            )
+    , Form.example
+        |> UsageExample.wrapMsg FormMsg
+            (\msg ->
+                case msg of
+                    FormMsg childMsg ->
+                        Just childMsg
+
+                    _ ->
+                        Nothing
+            )
+        |> UsageExample.wrapState FormState
+            (\msg ->
+                case msg of
+                    FormState childState ->
+                        Just childState
+
+                    _ ->
+                        Nothing
             )
     ]
 
 
 type State
     = ClickableCardWithTooltipState ClickableCardWithTooltip.State
+    | FormState Form.State
 
 
 type Msg
     = ClickableCardWithTooltipMsg ClickableCardWithTooltip.Msg
+    | FormMsg Form.Msg
