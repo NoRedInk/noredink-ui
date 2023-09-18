09b1fc4b076b8de3a589dcc12666d43883b48eed
Casey Webb <notcaseywebb@gmail.com>
2023-09-18 17:33:31 -0500

    Add FocusLoop.lazy

--- src/Nri/Ui/FocusLoop/V1.elm
+++ src/Nri/Ui/FocusLoop/V1.elm
@@ -1,12 +1,146 @@
-module Nri.Ui.FocusLoop.V1 exposing (addEvents)
+module Nri.Ui.FocusLoop.V1 exposing (addEvents, lazy, lazy2, lazy3, lazy4, lazy5)
 
-{-| Sometimes, there are sets of interactive elements that we want users to be able to navigate through with arrow keys rather than with tabs, and we want the final focus change to wrap. This module makes it easier to set up this focus and wrapping behavior.
+{-| Sometimes, there are sets of interactive elements that we want users to be able to navigate
+through with arrow keys rather than with tabs, and we want the final focus change to wrap.
+This module makes it easier to set up this focus and wrapping behavior.
 
-@docs addEvents
+@docs addEvents, lazy, lazy2, lazy3, lazy4, lazy5
 
 -}
 
+import Accessibility.Styled exposing (Html)
 import Accessibility.Styled.Key as Key exposing (Event)
+import Html.Styled.Lazy as Lazy
+
+
+{-| -}
+lazy :
+    { id : a -> String
+    , focus : String -> msg
+    , view : List (Event msg) -> a -> Html msg
+    , leftRight : Bool
+    , upDown : Bool
+    }
+    -> List a
+    -> List (Html msg)
+lazy config items =
+    case items of
+        [] ->
+            []
+
+        item :: [] ->
+            [ Lazy.lazy2 config.view [] item ]
+
+        _ ->
+            List.map
+                (\( item_, ( prev, next ) ) ->
+                    Lazy.lazy3 (view config) (config.id prev) (config.id next) item_
+                )
+                (siblings items)
+
+
+{-| -}
+lazy2 :
+    { id : ( a1, a2 ) -> String
+    , focus : String -> msg
+    , view : List (Event msg) -> a1 -> a2 -> Html msg
+    , leftRight : Bool
+    , upDown : Bool
+    }
+    -> List ( a1, a2 )
+    -> List (Html msg)
+lazy2 config items =
+    case items of
+        [] ->
+            []
+
+        ( a1, a2 ) :: [] ->
+            [ Lazy.lazy3 config.view [] a1 a2 ]
+
+        _ ->
+            List.map
+                (\( ( a1, a2 ), ( prev, next ) ) ->
+                    Lazy.lazy4 (view config) (config.id prev) (config.id next) a1 a2
+                )
+                (siblings items)
+
+
+{-| -}
+lazy3 :
+    { id : ( a1, a2, a3 ) -> String
+    , focus : String -> msg
+    , view : List (Event msg) -> a1 -> a2 -> a3 -> Html msg
+    , leftRight : Bool
+    , upDown : Bool
+    }
+    -> List ( a1, a2, a3 )
+    -> List (Html msg)
+lazy3 config items =
+    case items of
+        [] ->
+            []
+
+        ( a1, a2, a3 ) :: [] ->
+            [ Lazy.lazy4 config.view [] a1 a2 a3 ]
+
+        _ ->
+            List.map
+                (\( ( a1, a2, a3 ), ( prev, next ) ) ->
+                    Lazy.lazy5 (view config) (config.id prev) (config.id next) a1 a2 a3
+                )
+                (siblings items)
+
+
+{-| -}
+lazy4 :
+    { id : { a1 : a1, a2 : a2, a3 : a3, a4 : a4 } -> String
+    , focus : String -> msg
+    , view : List (Event msg) -> a1 -> a2 -> a3 -> a4 -> Html msg
+    , leftRight : Bool
+    , upDown : Bool
+    }
+    -> List { a1 : a1, a2 : a2, a3 : a3, a4 : a4 }
+    -> List (Html msg)
+lazy4 config items =
+    case items of
+        [] ->
+            []
+
+        { a1, a2, a3, a4 } :: [] ->
+            [ Lazy.lazy5 config.view [] a1 a2 a3 a4 ]
+
+        _ ->
+            List.map
+                (\( { a1, a2, a3, a4 }, ( prev, next ) ) ->
+                    Lazy.lazy6 (view config) (config.id prev) (config.id next) a1 a2 a3 a4
+                )
+                (siblings items)
+
+
+{-| -}
+lazy5 :
+    { id : { a1 : a1, a2 : a2, a3 : a3, a4 : a4, a5 : a5 } -> String
+    , focus : String -> msg
+    , view : List (Event msg) -> a1 -> a2 -> a3 -> a4 -> a5 -> Html msg
+    , leftRight : Bool
+    , upDown : Bool
+    }
+    -> List { a1 : a1, a2 : a2, a3 : a3, a4 : a4, a5 : a5 }
+    -> List (Html msg)
+lazy5 config items =
+    case items of
+        [] ->
+            []
+
+        { a1, a2, a3, a4, a5 } :: [] ->
+            [ Lazy.lazy6 config.view [] a1 a2 a3 a4 a5 ]
+
+        _ ->
+            List.map
+                (\( { a1, a2, a3, a4, a5 }, ( prev, next ) ) ->
+                    Lazy.lazy7 (view config) (config.id prev) (config.id next) a1 a2 a3 a4 a5
+                )
+                (siblings items)
 
 
 {-| -}
@@ -30,8 +164,22 @@ addEvents config items =
                 |> List.map (Tuple.mapSecond (keyEvents config))
 
 
+view :
+    { config
+        | view : List (Event msg) -> html
+        , focus : a -> msg
+        , leftRight : Bool
+        , upDown : Bool
+    }
+    -> a
+    -> a
+    -> html
+view config prevId nextId =
+    config.view (keyEvents config ( prevId, nextId ))
+
+
 keyEvents :
-    { focus : a -> msg, leftRight : Bool, upDown : Bool }
+    { config | focus : a -> msg, leftRight : Bool, upDown : Bool }
     -> ( a, a )
     -> List (Event msg)
 keyEvents config ( prev, next ) =
