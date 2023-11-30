fd392e74e7973616e9310508ed0115b1b1ba3203
Alex Perkins <alex.perkins@noredink.com>
2023-11-29 17:32:34 -0700

    test that SVGs show up

---
+++ tests/Spec/Nri/Ui/Container.elm
@@ -0,0 +1,46 @@
+module Spec.Nri.Ui.Container exposing (spec)
+
+import Html.Styled as Html exposing (..)
+import Nri.Ui.AssignmentIcon.V2 as AssignmentIcon
+import Nri.Ui.Container.V2 as Container
+import ProgramTest exposing (..)
+import Spec.TabsInternalHelpers exposing (..)
+import Test exposing (..)
+import Test.Html.Selector as Selector
+
+
+spec : Test
+spec =
+    describe "Nri.Ui.Container.V2"
+        [ describe "rendering" renderingTests
+        ]
+
+
+renderingTests : List Test
+renderingTests =
+    [ test "renders a container with text content" <|
+        \() ->
+            program [ Container.plaintext "hi" ]
+                |> ensureViewHas [ Selector.text "hi" ]
+                |> ensureViewHasNot [ Selector.tag "svg" ]
+                |> done
+    , test "renders a container with a top left icon content" <|
+        \() ->
+            program [ Container.topLeftIcon AssignmentIcon.dailyWritingCircled ]
+                |> ensureViewHas [ Selector.tag "svg" ]
+                |> done
+    ]
+
+
+type alias TestContext msg =
+    ProgramTest () msg ()
+
+
+program : List (Container.Attribute msg) -> TestContext msg
+program attrs =
+    ProgramTest.createSandbox
+        { init = ()
+        , update = \_ _ -> ()
+        , view = \() -> Container.view attrs |> Html.toUnstyled
+        }
+        |> ProgramTest.start ()
