87dce8c6ad854867689b920164d927fafd3ccaf8
Tessa Kelly <tessakkelly@gmail.com>
2024-02-08 18:10:40 -0700

    Reorder the settings for better legibility and to match the example code

--- component-catalog/src/IconExamples.elm
+++ component-catalog/src/IconExamples.elm
@@ -338,22 +338,48 @@ viewCustomizableExample groups state =
     Html.div
         [ Attributes.css
             [ Css.displayFlex
-            , Css.justifyContent Css.spaceBetween
-            , Css.alignItems Css.center
-            , Css.flexWrap Css.wrap
+            , Css.flexDirection Css.column
+            , Css.property "gap" "10px"
             ]
         ]
-        [ TextInput.view "Title"
-            [ TextInput.value state.label
-            , TextInput.text SetLabel
-            , TextInput.guidance "Required for non-decorative/meaningful icons"
+        [ inputGroup
+            [ Select.view "Icon"
+                [ Select.groupedChoices Tuple.first
+                    (List.map svgGroupedChoices groups)
+                , Select.value (Just state.icon)
+                ]
+                |> Html.map SetIcon
+            , TextInput.view "Title"
+                [ TextInput.value state.label
+                , TextInput.text SetLabel
+                , TextInput.guidance "Title is required for non-decorative/meaningful icons"
+                ]
             ]
-        , Select.view "Icon"
-            [ Select.groupedChoices Tuple.first
-                (List.map svgGroupedChoices groups)
-            , Select.value (Just state.icon)
+        , inputGroup
+            [ colorPicker state.color
+            , Html.label [ css [ Css.alignSelf Css.center ] ]
+                [ Html.text "Width: "
+                , Html.input
+                    [ Attributes.type_ "range"
+                    , Attributes.min "0"
+                    , Attributes.max "200"
+                    , Attributes.value (String.fromFloat state.width)
+                    , Events.onInput (SetWidth << String.toFloat)
+                    ]
+                    []
+                ]
+            , Html.label [ css [ Css.alignSelf Css.center ] ]
+                [ Html.text "Height: "
+                , Html.input
+                    [ Attributes.type_ "range"
+                    , Attributes.min "0"
+                    , Attributes.max "200"
+                    , Attributes.value (String.fromFloat state.height)
+                    , Events.onInput (SetHeight << String.toFloat)
+                    ]
+                    []
+                ]
             ]
-            |> Html.map SetIcon
         , Checkbox.view
             { label = "Show border"
             , selected = Checkbox.selectedFromBool state.showBorder
@@ -361,78 +387,77 @@ viewCustomizableExample groups state =
             [ Checkbox.id "show-border"
             , Checkbox.onCheck SetBorder
             ]
-        , Html.div [ css [ Css.position Css.relative, Css.paddingTop (Css.px InputStyles.defaultMarginTop) ] ]
-            [ InputLabelInternal.view
-                { for = "color-picker"
-                , label = "Color"
-                , theme = InputStyles.Standard
-                }
-                { error = InputErrorAndGuidanceInternal.noError
-                , noMarginTop = False
-                , hideLabel = False
-                , disabled = False
-                }
-            , Html.input
-                [ Attributes.id "color-picker"
-                , Attributes.type_ "color"
-                , Attributes.value (SolidColor.toHex state.color)
-                , Events.onInput (SetColor << SolidColor.fromHex)
-                , Attributes.list "noredink-ui-colors"
-                , css
-                    [ Css.border3 (Css.px 1) Css.solid Colors.gray75
-                    , Css.borderBottomWidth (Css.px 3)
-                    , Css.borderRadius (Css.px 8)
-                    , Css.focus
-                        [ Css.borderColor Colors.azure
-                        , Css.borderRadius (Css.px 8) |> Css.important
-                        ]
-                    , Css.color Colors.gray20
-                    , Css.backgroundColor Colors.white
-                    , Css.minWidth (Css.px 94)
-                    , Css.height (Css.px 45)
-                    , Css.padding4 (Css.px 7) (Css.px 30) (Css.px 5) (Css.px 7)
+        ]
 
-                    -- Dropdown Arrow
-                    --
-                    -- "appearance: none" removes the default dropdown arrows
-                    , VendorPrefixed.property "appearance" "none"
-                    , """<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="16px" height="16px" viewBox="0 0 25 15"><g fill=" """
-                        ++ SolidColor.toRGBString (fromCssColor Colors.azure)
-                        ++ """ "><path transform="rotate(270) translate(-20)" d="M19.2677026,20.7322696 C20.2443584,21.7070736 20.2443584,23.2915005 19.2677026,24.2677859 C18.7788191,24.7555583 18.139567,25 17.4999444,25 C16.8603219,25 16.2210698,24.7555583 15.7321863,24.2677859 L5.73229742,14.267897 C4.7556416,13.293093 4.7556416,11.7086662 5.73229742,10.7323808 L15.7321863,0.732491861 C16.7084718,-0.244163954 18.2914171,-0.244163954 19.2677026,0.732491861 C20.2443584,1.70729584 20.2443584,3.29172268 19.2677026,4.26800813 L11.0359422,12.5001389 L19.2677026,20.7322696 Z" ></path></g></svg> """
-                        |> urlUtf8
-                        |> Css.property "background"
-                    , Css.backgroundRepeat Css.noRepeat
-                    , Css.property "background-position" "center right -20px"
-                    , Css.backgroundOrigin Css.contentBox
+
+inputGroup : List (Html msg) -> Html msg
+inputGroup =
+    Html.div
+        [ css
+            [ Css.displayFlex
+            , Css.justifyContent Css.spaceBetween
+            , Css.flexWrap Css.wrap
+            ]
+        ]
+
+
+colorPicker : SolidColor -> Html Msg
+colorPicker selectedColor =
+    Html.div
+        [ css
+            [ Css.position Css.relative
+            , Css.paddingTop (Css.px InputStyles.defaultMarginTop)
+            , Css.display Css.inlineBlock
+            ]
+        ]
+        [ InputLabelInternal.view
+            { for = "color-picker"
+            , label = "Color"
+            , theme = InputStyles.Standard
+            }
+            { error = InputErrorAndGuidanceInternal.noError
+            , noMarginTop = False
+            , hideLabel = False
+            , disabled = False
+            }
+        , Html.input
+            [ Attributes.id "color-picker"
+            , Attributes.type_ "color"
+            , Attributes.value (SolidColor.toHex selectedColor)
+            , Events.onInput (SetColor << SolidColor.fromHex)
+            , Attributes.list "noredink-ui-colors"
+            , css
+                [ Css.border3 (Css.px 1) Css.solid Colors.gray75
+                , Css.borderBottomWidth (Css.px 3)
+                , Css.borderRadius (Css.px 8)
+                , Css.focus
+                    [ Css.borderColor Colors.azure
+                    , Css.borderRadius (Css.px 8) |> Css.important
                     ]
+                , Css.color Colors.gray20
+                , Css.backgroundColor Colors.white
+                , Css.minWidth (Css.px 94)
+                , Css.height (Css.px 45)
+                , Css.padding4 (Css.px 7) (Css.px 30) (Css.px 5) (Css.px 7)
+
+                -- Dropdown Arrow
+                --
+                -- "appearance: none" removes the default dropdown arrows
+                , VendorPrefixed.property "appearance" "none"
+                , """<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="16px" height="16px" viewBox="0 0 25 15"><g fill=" """
+                    ++ SolidColor.toRGBString (fromCssColor Colors.azure)
+                    ++ """ "><path transform="rotate(270) translate(-20)" d="M19.2677026,20.7322696 C20.2443584,21.7070736 20.2443584,23.2915005 19.2677026,24.2677859 C18.7788191,24.7555583 18.139567,25 17.4999444,25 C16.8603219,25 16.2210698,24.7555583 15.7321863,24.2677859 L5.73229742,14.267897 C4.7556416,13.293093 4.7556416,11.7086662 5.73229742,10.7323808 L15.7321863,0.732491861 C16.7084718,-0.244163954 18.2914171,-0.244163954 19.2677026,0.732491861 C20.2443584,1.70729584 20.2443584,3.29172268 19.2677026,4.26800813 L11.0359422,12.5001389 L19.2677026,20.7322696 Z" ></path></g></svg> """
+                    |> urlUtf8
+                    |> Css.property "background"
+                , Css.backgroundRepeat Css.noRepeat
+                , Css.property "background-position" "center right -20px"
+                , Css.backgroundOrigin Css.contentBox
                 ]
-                []
-            , Examples.Colors.all
-                |> List.map (\( name, color ) -> Html.option [ Attributes.value color.value ] [])
-                |> Html.datalist [ Attributes.id "noredink-ui-colors" ]
             ]
-        , Html.label []
-            [ Html.text "Width: "
-            , Html.input
-                [ Attributes.type_ "range"
-                , Attributes.min "0"
-                , Attributes.max "200"
-                , Attributes.value (String.fromFloat state.width)
-                , Events.onInput (SetWidth << String.toFloat)
-                ]
-                []
-            ]
-        , Html.label []
-            [ Html.text "Height: "
-            , Html.input
-                [ Attributes.type_ "range"
-                , Attributes.min "0"
-                , Attributes.max "200"
-                , Attributes.value (String.fromFloat state.height)
-                , Events.onInput (SetHeight << String.toFloat)
-                ]
-                []
-            ]
+            []
+        , Examples.Colors.all
+            |> List.map (\( name, color ) -> Html.option [ Attributes.value color.value ] [])
+            |> Html.datalist [ Attributes.id "noredink-ui-colors" ]
         ]
 
 
@@ -474,6 +499,11 @@ toCustomizableExampleResults state =
         ++ (Code.varWithTypeAnnotation "renderedSvg" "Html msg" <|
                 Code.pipelineMultiline
                     ([ Just <| state.renderSvgCode (Tuple.first state.icon)
+                     , if String.isEmpty state.label then
+                        Nothing
+
+                       else
+                        Just ("Svg.withLabel " ++ Code.string state.label)
                      , case selectedNriUiColor of
                         Just color ->
                             Just ("Svg.withColor Colors." ++ color)
@@ -491,11 +521,6 @@ toCustomizableExampleResults state =
 
                        else
                         Nothing
-                     , if String.isEmpty state.label then
-                        Nothing
-
-                       else
-                        Just ("Svg.withLabel " ++ Code.string state.label)
                      , Just "Svg.toHtml"
                      ]
                         |> List.filterMap identity
