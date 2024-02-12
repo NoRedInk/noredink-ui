2dbe6cdb42c1839536a600e713c626fd1f65f823
Tessa Kelly <tessakkelly@gmail.com>
2024-02-12 14:21:52 -0700

    :skull: remove theme instructions

--- component-catalog/src/Examples/Modal.elm
+++ component-catalog/src/Examples/Modal.elm
@@ -186,9 +186,6 @@ example =
     , about =
         Guidance.useATACGuide moduleName
             ++ [ Text.smallBody
-                    [ Text.markdown "Pair the `Modal.warning` theme with the `Button.danger` theme and the `Modal.info` theme with the `Button.primary` theme."
-                    ]
-               , Text.smallBody
                     [ Text.markdown "Use `Button.modal` and/or `ClickableText.modal` within the `Modal` footer." ]
                ]
     , view =
