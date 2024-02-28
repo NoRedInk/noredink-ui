af24384c5935ac8c1601317916ec1520df124515
Casey Webb <notcaseywebb@gmail.com>
2024-02-28 14:07:12 -0600

    Don't use extra styles on ul container'

--- src/Nri/Ui/SideNav/V5.elm
+++ src/Nri/Ui/SideNav/V5.elm
@@ -474,12 +474,9 @@ viewSidebarEntry config extraStyles entry_ =
                         ]
                     , ul
                         [ Attributes.css
-                            ([ listStyle none
-                             , padding zero
-                             , marginLeft zero |> Css.important
-                             ]
-                                ++ extraStyles
-                            )
+                            [ listStyle none
+                            , padding zero
+                            ]
                         , Aria.labelledBy id_
                         ]
                         (List.map
