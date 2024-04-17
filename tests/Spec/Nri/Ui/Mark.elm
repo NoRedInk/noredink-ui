c2855e19e671ea2998a3613c8431f64d57818eb4
Micah Hahn <micah.s.hahn@gmail.com>
2024-04-17 11:13:18 -0500

    Add more simple tests

--- tests/Spec/Nri/Ui/Mark.elm
+++ tests/Spec/Nri/Ui/Mark.elm
@@ -1,5 +1,6 @@
 module Spec.Nri.Ui.Mark exposing (..)
 
+import Css
 import Expect
 import Nri.Ui.Mark.V6 as Mark
 import Test exposing (..)
@@ -20,6 +21,66 @@ tests =
                             [ ( 1, Nothing, [] )
                             , ( 2, Nothing, [] )
                             , ( 3, Nothing, [] )
+                            ]
+            , test "mark on single segment" <|
+                \_ ->
+                    let
+                        startStyles =
+                            [ Css.color (Css.hex "000000") ]
+
+                        styles =
+                            [ Css.fontWeight Css.bold ]
+
+                        endStyles =
+                            [ Css.position Css.absolute ]
+                    in
+                    [ ( 1, [] )
+                    , ( 2
+                      , [ { name = Nothing
+                          , startStyles = startStyles
+                          , styles = styles
+                          , endStyles = endStyles
+                          }
+                        ]
+                      )
+                    , ( 3, [] )
+                    ]
+                        |> Mark.overlappingStyles
+                        |> Expect.equalLists
+                            [ ( 1, Nothing, [] )
+                            , ( 2, Nothing, startStyles ++ styles ++ endStyles )
+                            , ( 3, Nothing, [] )
+                            ]
+            , test "mark spanning multiple segments" <|
+                \_ ->
+                    let
+                        startStyles =
+                            [ Css.color (Css.hex "000000") ]
+
+                        styles =
+                            [ Css.fontWeight Css.bold ]
+
+                        endStyles =
+                            [ Css.position Css.absolute ]
+
+                        mark =
+                            { name = Nothing
+                            , startStyles = startStyles
+                            , styles = styles
+                            , endStyles = endStyles
+                            }
+                    in
+                    [ ( 1, [] )
+                    , ( 2, [ mark ] )
+                    , ( 3, [ mark ] )
+                    , ( 4, [] )
+                    ]
+                        |> Mark.overlappingStyles
+                        |> Expect.equalLists
+                            [ ( 1, Nothing, [] )
+                            , ( 2, Nothing, startStyles ++ styles )
+                            , ( 3, Nothing, styles ++ endStyles )
+                            , ( 4, Nothing, [] )
                             ]
             ]
         ]
