6b2cb0e035df85c500592496530c59e97650f887
Micah Hahn <micah.s.hahn@gmail.com>
2024-06-28 15:10:17 -0500

    Add "chooseColor" icon

--- component-catalog/src/Examples/UiIcon.elm
+++ component-catalog/src/Examples/UiIcon.elm
@@ -91,6 +91,7 @@ all =
         , ( "sync", UiIcon.sync, [] )
         , ( "delete", UiIcon.delete, [])
         , ( "addSticker", UiIcon.addSticker, [])
+        , ( "chooseColor", UiIcon.chooseColor, [])
         ]
       )
     , ( "Guidance"
