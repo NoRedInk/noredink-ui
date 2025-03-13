5117a9532ebe7967f4eb37180b1813013a4e611b
Brian J. Cardiff <bcardiff@gmail.com>
2025-03-13 15:08:50 -0300

    Add some more examples

--- component-catalog/src/UsageExamples/PerformancePlayground.elm
+++ component-catalog/src/UsageExamples/PerformancePlayground.elm
@@ -10,9 +10,13 @@ import Css
 import Debug.Control as Control exposing (Control)
 import Html.Styled exposing (..)
 import Nri.Ui.Button.V10 as Button
+import Nri.Ui.ClickableSvg.V2 as ClickableSvg
+import Nri.Ui.ClickableText.V4 as ClickableText
 import Nri.Ui.Heading.V3 as Heading
 import Nri.Ui.Spacing.V1 as Spacing
 import Nri.Ui.Text.V6 as Text
+import Nri.Ui.Tooltip.V3 as Tooltip
+import Nri.Ui.UiIcon.V1 as UiIcon
 import UsageExample exposing (UsageExample)
 
 
@@ -34,48 +38,113 @@ view : State -> List (Html Msg)
 view state =
     let
         currentValue =
-            Control.currentValue state
+            Control.currentValue state.config
+
+        controlView : Int -> Html Msg
+        controlView =
+            -- We return lambda to avoid caseing on each item while we loop
+            case currentValue.controlRendered of
+                PlainButton ->
+                    \i -> button [] [ text ("A button " ++ String.fromInt i) ]
+
+                PlainText ->
+                    \i -> span [] [ text ("A text " ++ String.fromInt i) ]
+
+                Button ->
+                    \i -> Button.button ("A button " ++ String.fromInt i) []
+
+                ClickableText ->
+                    \i -> ClickableText.button ("A button " ++ String.fromInt i) []
+
+                ClickableSvg ->
+                    \i -> ClickableSvg.button ("clickable-svg" ++ String.fromInt i) UiIcon.gear []
+
+                Tooltip ->
+                    \i ->
+                        Tooltip.view
+                            { id = "tooltip-" ++ String.fromInt i
+                            , trigger =
+                                \attributes ->
+                                    ClickableText.button ("Text with tooltip " ++ String.fromInt i) []
+                            }
+                            [ Tooltip.plaintext "This is a tooltip."
+                            , Tooltip.open (state.openTooltip == Just i)
+                            , Tooltip.onToggle (ToggleTooltip i)
+                            , Tooltip.onTop
+                            , Tooltip.smallPadding
+                            , Tooltip.fitToContent
+                            ]
     in
-    [ Control.view UpdateSettings state
+    [ Control.view UpdateSettings state.config
     , Heading.h2
         [ Heading.plaintext "Playground"
         , Heading.css [ Css.marginTop Spacing.verticalSpacerPx ]
         ]
     , div []
-        (List.repeat currentValue.buttonCount ()
-            |> List.indexedMap
-                (\i _ ->
-                    Button.button ("A button " ++ String.fromInt i) []
-                )
+        (List.repeat currentValue.controlCount ()
+            |> List.indexedMap (\i _ -> controlView i)
         )
     ]
 
 
+type ControlRendered
+    = PlainButton
+    | PlainText
+    | Button
+    | ClickableText
+    | ClickableSvg
+    | Tooltip
+
+
 type alias Config =
-    { buttonCount : Int
+    { controlRendered : ControlRendered
+    , controlCount : Int
     }
 
 
 {-| -}
 type alias State =
-    Control Config
+    { config : Control Config
+    , openTooltip : Maybe Int
+    }
 
 
 init : State
 init =
-    Control.record Config
-        |> Control.field "button count" (Control.int 0)
+    { config =
+        Control.record Config
+            |> Control.field "Control"
+                (Control.choice
+                    [ ( "Plain button", Control.value PlainButton )
+                    , ( "Plain text", Control.value PlainText )
+                    , ( "Nri.Ui.Button", Control.value Button )
+                    , ( "Nri.Ui.ClickableText", Control.value ClickableText )
+                    , ( "Nri.Ui.ClickableSvg", Control.value ClickableSvg )
+                    , ( "Nri.Ui.Tooltip", Control.value Tooltip )
+                    ]
+                )
+            |> Control.field "Count" (Control.int 1)
+    , openTooltip = Nothing
+    }
 
 
 {-| -}
 type Msg
-    = UpdateSettings State
+    = UpdateSettings (Control Config)
+    | ToggleTooltip Int Bool
 
 
 update : Msg -> State -> ( State, Cmd Msg )
-update msg _ =
+update msg state =
     case msg of
-        UpdateSettings newState ->
-            ( newState
+        UpdateSettings newConfig ->
+            ( { state | config = newConfig, openTooltip = Nothing }
             , Cmd.none
             )
+
+        ToggleTooltip i isOpen ->
+            if isOpen then
+                ( { state | openTooltip = Just i }, Cmd.none )
+
+            else
+                ( { state | openTooltip = Nothing }, Cmd.none )
